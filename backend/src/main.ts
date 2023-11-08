3fbc66d7fba993e1337052c680fbe1a1adb43b32
DongHyeok Lim <jh07050@gmail.com>
2023-11-08 17:37:52 +0900

    ✨ NestJS Swagger 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Morak')
+    .setDescription('Morak API description')
+    .setVersion('1.0')
+    .addTag('moraks')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
