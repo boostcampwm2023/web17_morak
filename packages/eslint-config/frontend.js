04a041d4f111c45f5d310057f3076ef634829669
LEEJW1953 <jiwonlee1953@naver.com>
2023-11-10 03:54:17 +0900

    :wrench: Frontend ESLint, Prettier, Stylelint 세팅

--- packages/eslint-config/frontend.js
+++ packages/eslint-config/frontend.js
@@ -1,3 +1,48 @@
 module.exports = {
-}
-  
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.js', '.jsx', '.ts', '.tsx'],
+    },
+  },
+  rules: {
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'index',
+          'object',
+          'type',
+          'unknown',
+        ],
+        pathGroups: [
+          {
+            pattern: '{react*, react*/**}',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
