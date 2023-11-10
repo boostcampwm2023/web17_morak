1bd7b5b678d23194e10805f0c54b2c038b493fcb
LEEJW1953 <jiwonlee1953@naver.com>
2023-11-10 16:11:45 +0900

    :wrench: Vite.config.ts 파일 ignorePatterns 추가

--- app/frontend/.eslintrc.cjs
+++ app/frontend/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     '@morak/eslint-config/frontend',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,6 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
