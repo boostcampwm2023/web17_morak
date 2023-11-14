25896fe432bebc360da9045d8add82e088864c02
Jiseung <js43o@naver.com>
2023-11-14 18:03:23 +0900

    💄 불필요한 fontWeight 필드 삭제

--- app/frontend/src/styles/fontFace.css.ts
+++ app/frontend/src/styles/fontFace.css.ts
@@ -2,30 +2,24 @@ import { fontFace } from '@vanilla-extract/css';
 
 export const pretendardRegular = fontFace({
   src: "url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')",
-  fontWeight: 400,
-});
-
-export const pretendardBold = fontFace({
-  src: "url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')",
-  fontWeight: 600,
 });
 
 export const pretendardSemibold = fontFace({
-  src: "url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')",
-  fontWeight: 700,
+  src: "url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff') format('woff')",
+});
+
+export const pretendardBold = fontFace({
+  src: "url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff')",
 });
 
 export const poppinsRegular = fontFace({
   src: 'url(https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJfecg.woff2) format("woff")',
-  fontWeight: 400,
 });
 
 export const poppinsSemibold = fontFace({
   src: 'url(https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLEj6Z1xlFQ.woff2) format("woff")',
-  fontWeight: 600,
 });
 
 export const poppinsBold = fontFace({
   src: 'url(https://fonts.gstatic.com/s/poppins/v20/pxiByp8kv8JHgFVrLCz7Z1xlFQ.woff2) format("woff")',
-  fontWeight: 700,
 });
