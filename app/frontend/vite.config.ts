13d43c86feb156161988da45804b006c5bb80202
ttaerrim <ltr0121@naver.com>
2023-11-14 15:00:18 +0900

    🎨 프로젝트 폴더 구조 절대 경로 세팅
    - vite.config.ts와 tsconfig.json 파일에 절대 경로 설정
    - eslint 파일 json 형식 변경, eslintignore 파일 추가
    - husky pre-commit 파일 수정 등
    
    Co-authored-by: LEEJW1953 <jiwonlee1953@naver.com>
    Co-authored-by: js43o <js43o@naver.com>

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@constants', replacement: path.resolve(__dirname, 'src/constants') },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@queries', replacement: path.resolve(__dirname, 'src/queries') },
+      { find: '@services', replacement: path.resolve(__dirname, 'src/services') },
+      { find: '@stores', replacement: path.resolve(__dirname, 'src/stores') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@types', replacement: path.resolve(__dirname, 'src/types') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+  },
 });
