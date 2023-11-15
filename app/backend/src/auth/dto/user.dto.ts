ee4c087bc5e729006ad4e369d32426378991cd4c
DongHyeok Lim <jh07050@gmail.com>
2023-11-15 14:24:32 +0900

    ✨ Auth Swagger 문서화 작업

--- app/backend/src/auth/dto/user.dto.ts
+++ app/backend/src/auth/dto/user.dto.ts
@@ -1,6 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, MaxLength, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @ApiProperty()
+  @IsString()
   provider_id: string;
+
+  @ApiProperty()
+  @IsEmail()
   email: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   nickname: string;
+
+  @ApiProperty()
+  @IsString()
   social_type: string;
+}
+
+export class LogoutDto {
+  @ApiProperty()
+  @IsString()
+  provider_id: string;
 }
