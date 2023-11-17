3aaee28cae40be93ab33deb01d6e8dbeb8d6f555
ttaerrim <ltr0121@naver.com>
2023-11-17 19:29:18 +0900

    📦️ vite-plugin-svgr 설치 및 설정

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+/// <reference types="vite-plugin-svgr/client" />
+import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
