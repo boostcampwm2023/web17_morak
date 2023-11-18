e78cbb12881a3b57df195ee845b528f1e9a06a7e
Jiseung <js43o@naver.com>
2023-11-18 18:01:56 +0900

    💄 Sidebar 컴포넌트 및 스타일링 코드 작성

---
+++ app/frontend/src/components/commons/Sidebar/index.css.ts
@@ -0,0 +1,41 @@
+import { vars } from '@styles';
+import { style } from '@vanilla-extract/css';
+
+export const wrapper = style({
+  position: 'absolute',
+  display: 'flex',
+  alignItems: 'center',
+  height: '100%',
+  transition: 'transform 0.5s',
+});
+
+export const closed = style({
+  transform: 'translateX(-40rem)',
+});
+
+export const panel = style({
+  display: 'flex',
+  width: '40rem',
+  height: '100%',
+  borderRight: `1px solid ${vars.color.grayscale100}`,
+});
+
+export const closeButton = style({
+  display: 'flex',
+  alignItems: 'center',
+  width: '2.4rem',
+  height: '4.8rem',
+  padding: 0,
+  border: `1px solid ${vars.color.grayscale200}`,
+  borderRadius: '0 0.8rem 0.8rem 0',
+  borderLeftColor: 'transparent',
+  background: vars.color.grayscaleWhite,
+  cursor: 'pointer',
+
+  ':hover': {
+    filter: 'brightness(1.1)',
+  },
+  ':active': {
+    filter: 'brightness(0.9)',
+  },
+});
