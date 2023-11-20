1bd0e8aef0faf22ff0b76156be3c65dc473035f8
ttaerrim <ltr0121@naver.com>
2023-11-20 16:34:36 +0900

    ♻️ 팝오버 컴포넌트에 class를 주입하여 사용할 수 있도록 props 추가

--- app/frontend/src/components/commons/Popover/index.tsx
+++ app/frontend/src/components/commons/Popover/index.tsx
@@ -5,8 +5,9 @@ import * as styles from './index.css';
 type PopoverProps = {
   children: ReactNode;
   type?: 'left' | 'center' | 'right';
+  className?: string;
 };
 
-export function Popover({ children, type = 'center' }: PopoverProps) {
-  return <div className={`${styles[type]} ${styles.container}`}>{children}</div>;
+export function Popover({ children, type = 'center', className }: PopoverProps) {
+  return <div className={`${styles[type]} ${styles.container} ${className || ''}`}>{children}</div>;
 }
