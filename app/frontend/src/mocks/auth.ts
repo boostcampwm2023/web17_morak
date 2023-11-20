ef0a22cff2a558bca16aa1e07dabeef4eea3896d
ttaerrim <ltr0121@naver.com>
2023-11-20 17:52:47 +0900

    🤡 /member/me mock 코드 작성

---
+++ app/frontend/src/mocks/auth.ts
@@ -0,0 +1,12 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('/member/me', () =>
+    HttpResponse.json({
+      providerId: '1234',
+      email: 'ttrrr121@gmail.com',
+      nickname: 'RIMI',
+      profilePicture: 'https://lh3.googleusercontent.com/ogw/AKPQZvwTnGmjh0sydCnI53wZMYLcKf-KZJ7Z9MaLg1ZVLQ=s64-c-mo',
+    }),
+  ),
+];
