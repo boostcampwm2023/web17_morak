83f5e1bdd8d44a1ccfb322718b99830ffcff8cd3
DongHyeok Lim <jh07050@gmail.com>
2023-11-21 17:22:48 +0900

    ✨ 모각코 CRUD 초안 설계
    모각코 CRUD 초안 설계입니다.
    사용자 인증에 대한 로직이 필요합니다.

--- app/backend/src/mogaco/mogaco.controller.ts
+++ app/backend/src/mogaco/mogaco.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { MogacoService } from './mogaco.service';
 import { Mogaco } from '@prisma/client';
+import { CreateMogacoDto, MogacoDto } from './dto';
+import { MogacoStatusValidationPipe } from './pipes/mogaco-status-validation.pipe';
+import { MogacoStatus } from './dto/mogaco-status.enum';
 
 @Controller('mogaco')
 export class MogacoController {
@@ -12,7 +15,25 @@ export class MogacoController {
   }
 
   @Get('/:id')
-  async getMogacoById(@Param('id', ParseIntPipe) id: number): Promise<Mogaco> {
+  async getMogacoById(@Param('id', ParseIntPipe) id: number): Promise<MogacoDto> {
     return this.mogacoService.getMogacoById(id);
+  }
+
+  @Post('/')
+  async createMogaco(@Body() createMogacoDto: CreateMogacoDto): Promise<Mogaco> {
+    return this.mogacoService.createMogaco(createMogacoDto);
+  }
+
+  @Delete('/:id')
+  async deleteMogaco(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.mogacoService.deleteMogaco(id);
+  }
+
+  @Patch('/:id/status')
+  updateMogacoStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', MogacoStatusValidationPipe) status: MogacoStatus,
+  ): Promise<Mogaco> {
+    return this.mogacoService.updateMogacoStatus(id, status);
   }
 }
--- app/backend/src/mogaco/mogaco.service.ts
+++ app/backend/src/mogaco/mogaco.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { MogacoRepository } from './mogaco.repository';
 import { Mogaco } from '@prisma/client';
+import { CreateMogacoDto, MogacoDto } from './dto';
+import { MogacoStatus } from './dto/mogaco-status.enum';
 
 @Injectable()
 export class MogacoService {
@@ -10,7 +12,21 @@ export class MogacoService {
     return this.mogacoRepository.getAllMogaco();
   }
 
-  async getMogacoById(id: number): Promise<Mogaco> {
+  async getMogacoById(id: number): Promise<MogacoDto> {
     return this.mogacoRepository.getMogacoById(id);
+  }
+
+  async createMogaco(createMogaco: CreateMogacoDto): Promise<Mogaco> {
+    return this.mogacoRepository.createMogaco(createMogaco);
+  }
+
+  async deleteMogaco(id: number): Promise<void> {
+    return this.mogacoRepository.deleteMogaco(id);
+  }
+
+  async updateMogacoStatus(id: number, status: MogacoStatus): Promise<Mogaco> {
+    const mogaco = await this.getMogacoById(id);
+    mogaco.status = status;
+    return this.mogacoRepository.updateMogacoStatus(mogaco);
   }
 }
