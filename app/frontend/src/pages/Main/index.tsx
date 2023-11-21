ad5a905d5aa1a2c35f4afc22272b96ff11df963e
ttaerrim <ltr0121@naver.com>
2023-11-21 10:43:23 +0900

    💄 유저 정보가 있으면 로그인 버튼 숨김

--- app/frontend/src/pages/Main/index.tsx
+++ app/frontend/src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import { ReactComponent as GoogleIcon } from '@/assets/icons/google.svg';
 import { Button } from '@/components';
 import { MAIN_IMAGE } from '@/constants';
+import { useUserAtom } from '@/stores';
 
 import * as styles from './index.css';
 
@@ -8,6 +9,7 @@ export function Main() {
   const onClickGoogleLogin = () => {
     window.location.href = `${import.meta.env.VITE_API_URL}/auth/google/login`;
   };
+  const [user] = useUserAtom();
 
   return (
     <div className={styles.container}>
@@ -20,16 +22,18 @@ export function Main() {
           모락과 함께하세요
         </div>
         <div className={styles.login}>
-          <Button
-            type="button"
-            theme="primary"
-            shape="line"
-            size="large"
-            onClick={onClickGoogleLogin}
-          >
-            <GoogleIcon />
-            Google로 시작하기
-          </Button>
+          {!user && (
+            <Button
+              type="button"
+              theme="primary"
+              shape="line"
+              size="large"
+              onClick={onClickGoogleLogin}
+            >
+              <GoogleIcon />
+              Google로 시작하기
+            </Button>
+          )}
         </div>
       </div>
     </div>
