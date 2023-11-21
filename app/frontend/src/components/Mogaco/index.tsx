3a31c86329ed9eebff3a1cae3b9ff7100b533764
ttaerrim <ltr0121@naver.com>
2023-11-21 16:36:21 +0900

    💄 모각코 리스트 페이지 글 리스트 컴포넌트 추가

--- app/frontend/src/components/Mogaco/index.tsx
+++ app/frontend/src/components/Mogaco/index.tsx
@@ -1,10 +1,12 @@
 import * as styles from './index.css';
+import { MogacoList } from './MogacoList';
 import { MogacoListHeader } from './MogacoListHeader';
 
 export function MogacoPage() {
   return (
     <div className={styles.container}>
       <MogacoListHeader />
+      <MogacoList />
     </div>
   );
 }
