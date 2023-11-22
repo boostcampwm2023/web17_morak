565a586fd52738261234cb4b20485bd9867becf8
Jiseung <js43o@naver.com>
2023-11-22 11:28:19 +0900

    ♻️ 헤더 버튼 영역 컴포넌트 분리

--- app/frontend/src/components/Detail/DetailHeader.tsx
+++ app/frontend/src/components/Detail/DetailHeader.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/commons/Button';
 import { sansBold24 } from '@/styles/font.css';
 
+import { DetailHeaderButtons } from './DetailHeaderButtons';
 import * as styles from './index.css';
 
 type DetailHeaderProps = {
@@ -11,34 +11,7 @@ export function DetailHeader({ state }: DetailHeaderProps) {
   return (
     <div className={styles.header}>
       <div className={sansBold24}>모각코 함께 해요</div>
-      <div className={styles.buttons}>
-        {
-          // eslint-disable-next-line no-nested-ternary
-          state === 'not-participated' ? (
-            <Button theme="primary" shape="fill" size="large">
-              참석하기
-            </Button>
-          ) : state === 'participated' ? (
-            <>
-              <Button theme="primary" shape="fill" size="large">
-                채팅
-              </Button>
-              <Button theme="primary" shape="line" size="large">
-                참석 취소
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button theme="primary" shape="line" size="large">
-                수정
-              </Button>
-              <Button theme="danger" shape="line" size="large">
-                삭제
-              </Button>
-            </>
-          )
-        }
-      </div>
+      <DetailHeaderButtons state={state} />
     </div>
   );
 }
