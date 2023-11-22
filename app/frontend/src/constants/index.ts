d6f4aeb7651f4b9860a8f1ab70ecc2f4a0087867
ttaerrim <ltr0121@naver.com>
2023-11-22 17:07:17 +0900

    ♻️ 프론트엔드의 url path 관리하는 상수 파일 추가

--- app/frontend/src/constants/index.ts
+++ app/frontend/src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './Header';
 export * from './Main';
 export * from './MogacoDetail';
+export * from './path';
---
+++ app/frontend/src/constants/path.ts
@@ -0,0 +1,5 @@
+export const PATH = {
+  MAIN: '/',
+  MOGACO: '/mogaco',
+  POST: '/post',
+};
