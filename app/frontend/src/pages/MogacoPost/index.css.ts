13b45aacb2af01c91dacff3f6b4a95f2575104f5
LEEJW1953 <jiwonlee1953@naver.com>
2023-11-22 09:22:30 +0900

    :sparkles: 모각코 게시글 작성 페이지 레이아웃 구현

---
+++ app/frontend/src/pages/MogacoPost/index.css.ts
@@ -0,0 +1,17 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '24px',
+  maxWidth: '75rem',
+  margin: '8rem auto 0',
+  padding: '1.5rem',
+});
+
+export const formContent = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.6rem',
+  width: '100%',
+});
---
+++ app/frontend/src/pages/MogacoPost/index.tsx
@@ -0,0 +1,59 @@
+import { Input, Button, Textarea } from '@/components';
+
+import * as styles from './index.css';
+
+export function MogacoPost() {
+  return (
+    <form className={styles.container}>
+      <div className={styles.formContent}>
+        <Input
+          label="제목"
+          placeholder="모각코 함께해요"
+          maxLength={64}
+          required
+        />
+      </div>
+      <div className={styles.formContent}>
+        <Input label="작성자" maxLength={64} required disabled />
+        <Input
+          label="그룹"
+          maxLength={64}
+          placeholder="그룹을 선택해주세요"
+          required
+        />
+        <Input
+          label="최대 인원 수"
+          type="number"
+          maxLength={64}
+          placeholder="20"
+          required
+        />
+        <Input
+          label="장소"
+          maxLength={64}
+          placeholder="장소를 검색해주세요"
+          required
+        />
+        <Input
+          label="날짜 및 시간"
+          type="datetime-local"
+          maxLength={64}
+          required
+        />
+        <Textarea label="설명" maxLength={1000} />
+      </div>
+      <div className={styles.formContent}>
+        <Button
+          type="submit"
+          theme="primary"
+          shape="fill"
+          size="large"
+          fullWidth
+          onClick={() => {}}
+        >
+          등록하기
+        </Button>
+      </div>
+    </form>
+  );
+}
