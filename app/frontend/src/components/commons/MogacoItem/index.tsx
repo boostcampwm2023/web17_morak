65250f12941359c4194b507f165d34f7863e78bf
ttaerrim <ltr0121@naver.com>
2023-11-22 16:39:41 +0900

    ♻️ 변경된 모각코 타입에 맞게 MogacoItem 컴포넌트 내용 변경

--- app/frontend/src/components/commons/MogacoItem/index.tsx
+++ app/frontend/src/components/commons/MogacoItem/index.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
+import dayjs from 'dayjs';
+
 import { ReactComponent as Calendar } from '@/assets/icons/calendar.svg';
 import { ReactComponent as Map } from '@/assets/icons/map.svg';
 import { ReactComponent as People } from '@/assets/icons/people.svg';
@@ -10,18 +12,18 @@ import * as styles from './index.css';
 
 export function MogacoItem({
   id,
-  disabled = false,
+  groupId,
   title,
-  group,
-  detail,
-  people,
-  maxPeople,
-  location,
+  contents,
   date,
+  participantList,
+  maxHumanCount,
+  address,
+  status,
 }: MogacoProps) {
   const MogacoLabel = (
-    <Label theme="primary" shape="fill" disabled={disabled}>
-      {disabled ? '모집 완료' : '모집 중'}
+    <Label theme="primary" shape="fill" disabled={status === '모집 완료'}>
+      {status}
     </Label>
   );
 
@@ -31,23 +33,25 @@ export function MogacoItem({
         {MogacoLabel}
         <div className={styles.title}>{title}</div>
       </div>
-      <div className={styles.group}>{group}</div>
+      <div className={styles.group}>{groupId}</div>
       <div className={styles.content}>
-        <div className={styles.detail}>{detail}</div>
+        <div className={styles.detail}>{contents}</div>
         <div className={styles.info}>
           <div className={styles.infoContent}>
             <People className={styles.icon} />
             <div className={styles.infoText}>
-              {people}/{maxPeople}
+              {participantList?.length || 0}/{maxHumanCount}
             </div>
           </div>
           <div className={styles.infoContent}>
             <Map className={styles.icon} />
-            <div className={styles.infoText}>{location}</div>
+            <div className={styles.infoText}>{address}</div>
           </div>
           <div className={styles.infoContent}>
             <Calendar className={styles.icon} />
-            <div className={styles.infoText}>{date}</div>
+            <div className={styles.infoText}>
+              {dayjs(date).format('YYYY/MM/DD HH:mm~')}
+            </div>
           </div>
         </div>
       </div>
