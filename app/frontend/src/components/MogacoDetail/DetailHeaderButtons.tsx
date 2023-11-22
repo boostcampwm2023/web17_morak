4d858cbcf837e4e5c86b1f89dd9f7ef87bff1753
Jiseung <js43o@naver.com>
2023-11-22 20:37:24 +0900

    ♻️ 버튼 컴포넌트 형식 결정 로직을 객체 형태로 변경

--- app/frontend/src/components/MogacoDetail/DetailHeaderButtons.tsx
+++ app/frontend/src/components/MogacoDetail/DetailHeaderButtons.tsx
@@ -4,40 +4,34 @@ type DetailHeaderButtonsProps = {
   status: string;
 };
 
-export function DetailHeaderButtons({ status }: DetailHeaderButtonsProps) {
-  if (status === '모집 중') {
-    return (
+const buttonComponents = {
+  '모집 중': (
+    <Button theme="primary" shape="fill" size="large">
+      참석하기
+    </Button>
+  ),
+  마감: (
+    <>
       <Button theme="primary" shape="fill" size="large">
-        참석하기
+        채팅
       </Button>
-    );
-  }
+      <Button theme="primary" shape="line" size="large">
+        참석 취소
+      </Button>
+    </>
+  ),
+  종료: (
+    <>
+      <Button theme="primary" shape="line" size="large">
+        수정
+      </Button>
+      <Button theme="danger" shape="line" size="large">
+        삭제
+      </Button>
+    </>
+  ),
+};
 
-  if (status === '마감') {
-    return (
-      <>
-        <Button theme="primary" shape="fill" size="large">
-          채팅
-        </Button>
-        <Button theme="primary" shape="line" size="large">
-          참석 취소
-        </Button>
-      </>
-    );
-  }
-
-  if (status === '종료') {
-    return (
-      <>
-        <Button theme="primary" shape="line" size="large">
-          수정
-        </Button>
-        <Button theme="danger" shape="line" size="large">
-          삭제
-        </Button>
-      </>
-    );
-  }
-
-  return null;
+export function DetailHeaderButtons({ status }: DetailHeaderButtonsProps) {
+  return status ? buttonComponents[status as '모집 중' | '마감' | '종료'] : '';
 }
