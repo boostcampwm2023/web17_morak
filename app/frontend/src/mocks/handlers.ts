2cc21e22161430565cc86b2a32e0b080eb90d6c1
ttaerrim <ltr0121@naver.com>
2023-11-22 16:33:42 +0900

    🤡 mogaco 모킹 API 추가

--- app/frontend/src/mocks/handlers.ts
+++ app/frontend/src/mocks/handlers.ts
@@ -1,3 +1,4 @@
 import { memberAPIHandlers } from './members';
+import { mogacoAPIHandlers } from './mogacos';
 
-export const mockAPIhandlers = [...memberAPIHandlers];
+export const mockAPIhandlers = [...memberAPIHandlers, ...mogacoAPIHandlers];
