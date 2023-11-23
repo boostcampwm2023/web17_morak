fbf7019351d024daeb0d13d8d73cbd9f95ed1555
Jiseung <js43o@naver.com>
2023-11-24 02:25:17 +0900

    🤡 mock 데이터 최신화

--- app/frontend/src/mocks/members.ts
+++ app/frontend/src/mocks/members.ts
@@ -1,19 +1,24 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { http, HttpResponse } from 'msw';
 
-const userList = [
+import { Member } from '@/types';
+
+const memberList: Member[] = [
   {
-    id: '1',
+    providerId: '1',
+    email: '.',
     nickname: '지승',
     profilePicture: 'https://avatars.githubusercontent.com/u/50646827?v=4',
   },
   {
-    id: '2',
+    providerId: '2',
+    email: '.',
     nickname: '지원',
     profilePicture: 'https://avatars.githubusercontent.com/u/110762136?v=4',
   },
   {
-    id: '3',
+    providerId: '3',
+    email: '.',
     nickname: '태림',
     profilePicture: 'https://avatars.githubusercontent.com/u/43867711?v=4',
   },
@@ -23,19 +28,13 @@ export const memberAPIHandlers = [
   http.get(
     '/member/me',
     // () => HttpResponse.error(),
-    () =>
-      HttpResponse.json({
-        providerId: '1234',
-        email: 'ttrrr121@gmail.com',
-        nickname: 'RIMI',
-        profilePicture:
-          'https://lh3.googleusercontent.com/ogw/AKPQZvwTnGmjh0sydCnI53wZMYLcKf-KZJ7Z9MaLg1ZVLQ=s64-c-mo',
-      }),
+    () => HttpResponse.json(memberList[0]),
   ),
   http.get(
     '/member/:id',
     // () => HttpResponse.error(),
-    ({ params: { id } }) => HttpResponse.json(userList[Number(id) - 1]),
+    ({ params: { id } }) => HttpResponse.json(memberList[Number(id) - 1]),
   ),
 ];
-export { userList };
+
+export { memberList };
--- app/frontend/src/mocks/mogaco.ts
+++ app/frontend/src/mocks/mogaco.ts
@@ -1,84 +1,84 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { http, HttpResponse } from 'msw';
 
-import { Mogaco, Participant } from '@/types';
+import { Member, Mogaco } from '@/types';
 
-import { userList } from './members';
+import { memberList } from './members';
 
-const mogacoList = [
+const mogacoList: Mogaco[] = [
   {
     id: '1',
     groupId: 1,
-    memberId: '1',
     title: '인천역 모각코',
     contents: '인천에서 같이 모각코 하실 분을 모십니다.',
     date: '2023-11-22T12:00:00.000Z',
     maxHumanCount: 5,
     address: '서울 관악구 어디길 어디로 뭐시기카페',
     status: '모집 중' as const,
+    member: memberList[0],
   },
   {
     id: '2',
     groupId: 1,
-    memberId: '2',
     title: '이수역 모각코',
     contents: '이수역 모각코 하실 분 구합니다!',
     date: '2023-11-22T12:00:00.000Z',
     maxHumanCount: 5,
     address: '주소주소주소주소주소',
     status: '모집 중' as const,
+    member: memberList[1],
   },
   {
     id: '3',
     groupId: 1,
-    memberId: '3',
     title: '종각역 모각코',
     contents: '종각역에서 모각코 하실 분 구해요',
     date: '2023-10-11T12:00:00.000Z',
     maxHumanCount: 5,
     address: '주소주소주소주소주소',
     status: '모집 중' as const,
+    member: memberList[2],
   },
   {
     id: '4',
     groupId: 1,
-    memberId: '1',
     title: '사당역 모각코',
     contents: '사당역 크레이저 커피로 오세요~',
     date: '2023-11-22T12:00:00.000Z',
     maxHumanCount: 5,
     address: '주소주소주소주소주소',
     status: '모집 중' as const,
+    member: memberList[1],
   },
 ];
 
-const participantsList = [
-  [userList[0], userList[1], userList[2]],
-  [userList[1], userList[2]],
-  [userList[0], userList[2]],
-  [userList[0], userList[1]],
+const participantsList: Member[][] = [
+  [memberList[0], memberList[1], memberList[2]],
+  [memberList[1], memberList[2]],
+  [memberList[0], memberList[2]],
+  [memberList[0], memberList[1]],
 ];
 
 export const mogacoAPIHandlers = [
   http.get('/mogaco', () => HttpResponse.json<Mogaco[]>(mogacoList)),
+  http.post('/mogaco', () => HttpResponse.json(null, { status: 200 })),
   http.delete('/mogaco/:id', () => HttpResponse.json(null, { status: 200 })),
   http.get('/mogaco/:id', ({ params: { id } }) =>
     HttpResponse.json<Mogaco>(mogacoList[Number(id) - 1]),
   ),
   http.get('/mogaco/:id/participants', ({ params: { id } }) =>
-    HttpResponse.json<Participant[]>(participantsList[Number(id) - 1]),
+    HttpResponse.json<Member[]>(participantsList[Number(id) - 1]),
   ),
-  http.post('/mogaco', () => HttpResponse.json(null, { status: 201 })),
   http.post('/mogaco/:id/join', ({ params: { id } }) => {
     participantsList[Number(id) - 1] = [
       ...participantsList[Number(id) - 1],
-      userList[0],
+      memberList[0],
     ];
     return HttpResponse.json(null, { status: 200 });
   }),
   http.delete('/mogaco/:id/join', ({ params: { id } }) => {
     participantsList[Number(id) - 1] = participantsList[Number(id) - 1].filter(
-      (participant) => participant.id !== userList[0].id,
+      (participant) => participant.providerId !== memberList[0].providerId,
     );
     return HttpResponse.json(null, { status: 200 });
   }),
