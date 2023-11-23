bbe50b0d72c6f17bb4a514ec7be49d2bdca7eae9
ttaerrim <ltr0121@naver.com>
2023-11-23 11:53:38 +0900

    ✨ 삭제 버튼 클릭 시 글을 삭제

--- app/frontend/src/components/MogacoDetail/DetailHeaderButtons.tsx
+++ app/frontend/src/components/MogacoDetail/DetailHeaderButtons.tsx
@@ -1,4 +1,7 @@
+import { useNavigate, useParams } from 'react-router-dom';
+
 import { Button } from '@/components';
+import { useDeleteMogacoQuery } from '@/queries/hooks';
 import { mogaco } from '@/services';
 
 type DetailHeaderButtonsProps = {
@@ -14,6 +17,29 @@ export function DetailHeaderButtons({
   userParticipated,
   status,
 }: DetailHeaderButtonsProps) {
+  const navigate = useNavigate();
+  const { id: postId } = useParams();
+  const { mutateAsync } = useDeleteMogacoQuery();
+
+  const handleDelete = async () => {
+    if (!postId) {
+      return;
+    }
+
+    const res = await mutateAsync(id);
+    if (res.status === 200) {
+      navigate('/mogaco');
+    }
+  };
+
+  const onClickDelete = () => {
+    // eslint-disable-next-line no-alert
+    const answer = window.confirm('삭제하시겠습니까?');
+    if (answer) {
+      handleDelete();
+    }
+  };
+
   const onClickJoin = async () => {
     await mogaco.join(id);
   };
@@ -28,7 +54,12 @@ export function DetailHeaderButtons({
         <Button theme="primary" shape="line" size="large">
           수정
         </Button>
-        <Button theme="danger" shape="line" size="large">
+        <Button
+          theme="danger"
+          shape="line"
+          size="large"
+          onClick={onClickDelete}
+        >
           삭제
         </Button>
       </>
