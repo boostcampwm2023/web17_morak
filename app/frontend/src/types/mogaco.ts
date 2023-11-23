233c9e3e3c66a7fa19e42e5a3a02ba7c0d1ce7fc
LEEJW1953 <jiwonlee1953@naver.com>
2023-11-23 15:29:26 +0900

    :label: Mogaco type 유틸리티 타입 사용

--- app/frontend/src/types/mogaco.ts
+++ app/frontend/src/types/mogaco.ts
@@ -1,4 +1,4 @@
-export type Mogaco = {
+interface Mogaco {
   id: string;
   groupId: string;
   memberId: string;
@@ -9,10 +9,32 @@ export type Mogaco = {
   maxHumanCount: number;
   address: string;
   status: '모집 중' | '마감' | '종료';
-};
-
-export type Participant = {
-  id: string;
   nickname: string;
   profile: string;
+}
+
+export type MogacoPostForm = {
+  title: string;
+  memberId: string;
+  groupId: string;
+  contents: string;
+  date: string;
+  maxHumanCount: string;
+  address: string;
 };
+
+export type MogacoProps = Pick<
+  Mogaco,
+  | 'id'
+  | 'groupId'
+  | 'memberId'
+  | 'title'
+  | 'contents'
+  | 'date'
+  | 'participantList'
+  | 'maxHumanCount'
+  | 'address'
+  | 'status'
+>;
+
+export type Participant = Pick<Mogaco, 'memberId' | 'nickname' | 'profile'>;
