3e5bd340e41e832a9ce2ecc36887dab40765b909
DongHyeok Lim <jh07050@gmail.com>
2023-11-27 10:57:10 +0900

    ♻️ GroupsDto id type: string -> bigint

--- app/backend/src/groups/dto/groups.dto.ts
+++ app/backend/src/groups/dto/groups.dto.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class GroupsDto implements ResponseGroupsDto {
   @ApiProperty({ description: 'ID of the Group', example: '1' })
-  id: string;
+  id: bigint;
 
   @ApiProperty({ description: 'title of the Group', example: '부스트캠프 웹・모바일 8기' })
   title: string;
--- packages/apitype/dto/response/groups.ts
+++ packages/apitype/dto/response/groups.ts
@@ -1,4 +1,4 @@
 export interface ResponseGroupsDto {
-  id: string;
+  id: bigint;
   title: string;
 }
