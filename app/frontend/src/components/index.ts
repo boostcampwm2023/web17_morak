ec0891d9a312a25c1ee9db29bb062c99c8843d49
ttaerrim <ltr0121@naver.com>
2023-11-27 14:32:12 +0900

    ♻️ 사이드바 내 모각코 정보 MogacoSideItem 컴포넌트로 분리

--- app/frontend/src/components/index.ts
+++ app/frontend/src/components/index.ts
@@ -11,4 +11,5 @@ export * from './Loading/LoadingIndicator';
 export * from './MogacoItem';
 export * from './Popover';
 export * from './Sidebar';
+export * from './Sidebar/Item/Mogaco';
 export * from './UserChip';
--- app/frontend/src/pages/Calendar/Sidebar.tsx
+++ app/frontend/src/pages/Calendar/Sidebar.tsx
@@ -1,57 +1,12 @@
 import { useState } from 'react';
 
-import dayjs from 'dayjs';
-
-import { ReactComponent as Calendar } from '@/assets/icons/calendar.svg';
-import { ReactComponent as Map } from '@/assets/icons/map.svg';
-import { ReactComponent as People } from '@/assets/icons/people.svg';
-import { Button, Label, Sidebar } from '@/components';
-
-import * as styles from './Sidebar.css';
+import { Sidebar, MogacoSidebarItem } from '@/components';
 
 export function CalendarSidebar() {
   const [closed, setClosed] = useState(true);
   return (
     <Sidebar closed={closed} toggleClosed={() => setClosed(!closed)}>
-      <div className={styles.container}>
-        <div className={styles.wrapper}>
-          <div className={styles.titleWrapper}>
-            <Label theme="primary" shape="fill">
-              모집 중
-            </Label>
-            <span className={styles.title}>모각코 함께 하기</span>
-          </div>
-          <span className={styles.group}>부스트캠프 웹모바일 8기</span>
-          <div className={styles.infoWrapper}>
-            <div className={styles.infoContent}>
-              <People className={styles.icon} />
-              <div className={styles.infoText}>2/5</div>
-            </div>
-            <div className={styles.infoContent}>
-              <Map className={styles.icon} />
-              <div className={styles.infoText}>
-                서울 관악구 남현3길 71 크레이저 커피
-              </div>
-            </div>
-            <div className={styles.infoContent}>
-              <Calendar className={styles.icon} />
-              <div className={styles.infoText}>
-                {dayjs('2023-11-7').format('YYYY/MM/DD HH:mm~')}
-              </div>
-            </div>
-          </div>
-          <p className={styles.contents}>
-            사당역에서 부스트캠프 모락 팀이 모입니다 사당역에서 부스트캠프 모락
-            팀이 모입니다 사당역에서 부스트캠프 모락 팀이 모입니다 사당역에서
-            부스트캠프 모락 팀이 모입니다 사당역에서 부스트캠프 모락 팀이
-            모입니다 사당역에서 부스트캠프 모락 팀이 모입니다 사당역에서
-            부스트캠프 모락 팀이 모입니다
-          </p>
-        </div>
-        <Button fullWidth shape="fill" theme="primary" size="large">
-          글로 이동하기
-        </Button>
-      </div>
+      <MogacoSidebarItem />
     </Sidebar>
   );
 }
