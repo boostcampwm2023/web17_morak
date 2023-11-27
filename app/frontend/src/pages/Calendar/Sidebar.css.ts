166fc3c0ba5e960efed8d378b2cfa7ed5e0e7b80
ttaerrim <ltr0121@naver.com>
2023-11-27 14:03:21 +0900

    ✨ 모각코 사이드바 구현

--- app/frontend/src/pages/Calendar/Sidebar.css.ts
+++ app/frontend/src/pages/Calendar/Sidebar.css.ts
@@ -1,5 +1,56 @@
 import { style } from '@vanilla-extract/css';
 
-import { sansBold24 } from '@/styles/font.css';
+import { vars } from '@/styles';
+import {
+  sansBold12,
+  sansBold24,
+  sansRegular12,
+  sansRegular16,
+} from '@/styles/font.css';
 
-export const container = style([sansBold24, { width: '100%' }]);
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  width: '100%',
+  color: vars.color.grayscaleBlack,
+  padding: '2rem',
+});
+export const contents = style([sansRegular16, {}]);
+export const group = style([sansBold12, {}]);
+export const icon = style({
+  fill: vars.color.grayscale200,
+});
+export const infoContent = style({
+  display: 'flex',
+  width: '100%',
+  alignItems: 'center',
+  gap: '0.4rem',
+});
+export const infoText = style([
+  sansRegular12,
+  {
+    width: '100%',
+    alignItems: 'center',
+    gap: '0.4rem',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+  },
+]);
+export const infoWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '0.4rem',
+});
+
+export const title = style([sansBold24, {}]);
+export const titleWrapper = style({
+  display: 'flex',
+  gap: '0.4rem',
+});
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1.6rem',
+});
