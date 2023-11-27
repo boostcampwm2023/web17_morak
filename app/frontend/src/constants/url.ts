bf555c75487a935ba2b3f9a800e644ca52e8b7f7
ttaerrim <ltr0121@naver.com>
2023-11-27 11:51:48 +0900

    ✨ local에서 API test를 위한 command 추가

--- app/frontend/src/constants/url.ts
+++ app/frontend/src/constants/url.ts
@@ -1,3 +1,6 @@
 export const URL = {
-  API: 'http://soma2.iptime.org:9991',
+  API:
+    import.meta.env.MODE === 'test'
+      ? 'http://localhost:8080'
+      : 'http://soma2.iptime.org:9991',
 };
