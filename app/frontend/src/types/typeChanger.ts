bad8ba0deaea77a61ae848be5f41de0493c5e44c
ttaerrim <ltr0121@naver.com>
2023-11-28 11:34:17 +0900

    ✨ JsonTypeChanger 추가
    - bigint와 Date는 string 타입으로 변환, 내부의 배열까지 재귀적으로 변환

--- app/frontend/src/types/typeChanger.ts
+++ app/frontend/src/types/typeChanger.ts
@@ -1,3 +1,14 @@
+// bigint와 Date를 string으로 변환하는 타입
+export type JsonTypeChanger<T> = {
+  [P in keyof T]: T[P] extends bigint | Date
+    ? string
+    : T[P] extends Array<infer U>
+    ? JsonTypeChanger<U>[]
+    : T[P] extends object
+    ? JsonTypeChanger<T[P]>
+    : T[P];
+};
+
 // Interface, Class 등을 Type으로 변환하는 타입
 export type TypeWrapper<T> = {
   [P in keyof T]: [T[P]];
