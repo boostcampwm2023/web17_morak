46a25ba9fd1fd412e2f4026ec5417d941957664b
ttaerrim <ltr0121@naver.com>
2023-11-28 11:32:25 +0900

    ✨ axios의 get, post, patch, delete wrapper를 만들어 사용

--- app/frontend/src/services/morakAPI.ts
+++ app/frontend/src/services/morakAPI.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import { URL } from '@/constants';
+import { JsonTypeChanger } from '@/types';
 import { getCookies } from '@/utils';
 
 const initialHeaders = {
@@ -11,7 +12,34 @@ const headers = getCookies('access_token')
   ? { ...initialHeaders, Authorization: `Bearer ${getCookies('access_token')}` }
   : initialHeaders;
 
-export const morakAPI = axios.create({
+const morakAxios = axios.create({
   baseURL: import.meta.env.MODE === 'development' ? '' : URL.API,
   headers,
 });
+
+const morakAPI = {
+  get: <TResponse>(
+    url: string,
+    config?: AxiosRequestConfig<unknown>,
+  ): Promise<AxiosResponse<JsonTypeChanger<TResponse>, unknown>> =>
+    morakAxios.get(url, config),
+  post: <TResponse, TRequest>(
+    url: string,
+    data?: TRequest,
+    config?: AxiosRequestConfig<unknown>,
+  ): Promise<AxiosResponse<JsonTypeChanger<TResponse>, unknown>> =>
+    morakAxios.post(url, data, config),
+  patch: <TResponse, TRequest>(
+    url: string,
+    data?: TRequest,
+    config?: AxiosRequestConfig<unknown>,
+  ): Promise<AxiosResponse<JsonTypeChanger<TResponse>, unknown>> =>
+    morakAxios.patch(url, data, config),
+  delete: <TResponse>(
+    url: string,
+    config?: AxiosRequestConfig<unknown>,
+  ): Promise<AxiosResponse<JsonTypeChanger<TResponse>, unknown>> =>
+    morakAxios.delete(url, config),
+};
+
+export { morakAPI };
