19797da08b92d3ca2f7a306f8e0d35a9bf951086
LEEJW1953 <jiwonlee1953@naver.com>
2023-11-28 15:42:13 +0900

    :sparkles: 지도의 마커 클릭 시 사이드바 표시

--- app/frontend/src/pages/Map/index.tsx
+++ app/frontend/src/pages/Map/index.tsx
@@ -1,12 +1,27 @@
-import { Header, Map } from '@/components';
+import { useState } from 'react';
+
+import { Header, Map, MogacoSideBar } from '@/components';
 
 import * as styles from './index.css';
 
 export function MapLayout() {
+  const [closedSidebar, setClosedSidebar] = useState(true);
+  const onClickCloseSidebar = () => {
+    setClosedSidebar(!closedSidebar);
+  };
+
   return (
     <div className={styles.container}>
       <Header />
-      <Map />
+      <Map
+        onClick={() => {
+          setClosedSidebar(false);
+        }}
+      />
+      <MogacoSideBar
+        closed={closedSidebar}
+        toggleClosed={onClickCloseSidebar}
+      />
     </div>
   );
 }
