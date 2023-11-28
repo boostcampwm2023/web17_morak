62d1ff945ad09b9f9e747da7ab08471caaf725a0
ttaerrim <ltr0121@naver.com>
2023-11-28 22:41:41 +0900

    🔨 모달 내부 로직 수정
    - dialog 내에 method=dialog인 form 안에서 버튼을 사용할 경우, ref를 사용하지 않고 모달을 닫을 수 있습니다
    - dialog의 open 값을 전역 상태 값으로 사용합니다

--- app/frontend/src/components/Modal/index.tsx
+++ app/frontend/src/components/Modal/index.tsx
@@ -1,10 +1,10 @@
+import { useModalAtom } from '@/stores';
 import { sansBold24, sansRegular16 } from '@/styles/font.css';
 
 import { Footer } from './Footer';
 import * as styles from './index.css';
 
 type ModalProps = {
-  dialogRef: React.RefObject<HTMLDialogElement>;
   title: string;
   content?: string;
   buttonType: 'single' | 'double';
@@ -14,7 +14,6 @@ type ModalProps = {
 };
 
 export function Modal({
-  dialogRef,
   title,
   content = '',
   buttonType,
@@ -22,24 +21,27 @@ export function Modal({
   cancelButtonText = '취소',
   onClickConfirm,
 }: ModalProps) {
+  const [open, setOpen] = useModalAtom();
+
   const closeModal = () => {
-    if (!dialogRef.current) return;
-    dialogRef.current.close();
+    setOpen(false);
   };
 
   return (
-    <dialog className={styles.container} ref={dialogRef}>
-      <div className={styles.textArea}>
-        <div className={sansBold24}>{title}</div>
-        {content && <div className={sansRegular16}>{content}</div>}
-      </div>
-      <Footer
-        buttonType={buttonType}
-        confirmButtonText={confirmButtonText}
-        cancelButtonText={cancelButtonText}
-        closeModal={closeModal}
-        onClickConfirm={onClickConfirm}
-      />
+    <dialog className={styles.container} open={open}>
+      <form method="dialog" className={styles.form}>
+        <div className={styles.textArea}>
+          <div className={sansBold24}>{title}</div>
+          {content && <div className={sansRegular16}>{content}</div>}
+        </div>
+        <Footer
+          buttonType={buttonType}
+          confirmButtonText={confirmButtonText}
+          cancelButtonText={cancelButtonText}
+          closeModal={closeModal}
+          onClickConfirm={onClickConfirm}
+        />
+      </form>
     </dialog>
   );
 }
