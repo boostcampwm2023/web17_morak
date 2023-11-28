38cb91781e553ac30533a53ad4be673348a1e944
ccxz84 <ccxz84@naver.com>
2023-11-28 14:58:23 +0900

    :sparkles: chat 모듈 정의

--- packages/morak-chat/src/server/decorator/message.decorator.ts
+++ packages/morak-chat/src/server/decorator/message.decorator.ts
@@ -1,5 +1,4 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { Socket } from 'socket.io';
 
 export const ChatMessage = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
