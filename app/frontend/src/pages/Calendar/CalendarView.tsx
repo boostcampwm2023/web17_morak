cceb7db73189db96d0af5f5b7d1e725ba1fa5b57
ttaerrim <ltr0121@naver.com>
2023-11-29 14:38:30 +0900

    🔨 달력에서 년-월을 받아 데이터 요청에 사용

--- app/frontend/src/pages/Calendar/CalendarView.tsx
+++ app/frontend/src/pages/Calendar/CalendarView.tsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
+
 import { EventClickArg } from '@fullcalendar/core/index.js';
 import koLocale from '@fullcalendar/core/locales/ko';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
+import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
 
+import { queryKeys } from '@/queries';
 import { vars } from '@/styles';
+import { Mogaco } from '@/types';
 
 import * as styles from './CalendarView.css';
 
@@ -12,27 +18,31 @@ export function CalendarView({
 }: {
   onClickEvent: (dayEvent: EventClickArg) => void;
 }) {
+  const [selectedMonth, setSelectedMonth] = useState(new Date());
+  const refineMogacoList = (mogacoList: Mogaco[]) =>
+    mogacoList.map((mogaco) => {
+      const { id, title, date } = mogaco;
+      return { id, title, date };
+    });
+
+  const { data: mogacoData } = useQuery({
+    ...queryKeys.mogaco.list({ date: dayjs(selectedMonth).format('YYYY-MM') }),
+    select: refineMogacoList,
+  });
+
   return (
     <div className={styles.container}>
       <FullCalendar
+        datesSet={(dateInfo) => setSelectedMonth(dateInfo.view.currentStart)}
         locale={koLocale}
         plugins={[dayGridPlugin]}
         initialView="dayGridMonth"
         weekends
-        events={[
-          {
-            title: '12:00 인천 모각코 이벤트',
-            date: '2023-11-22',
-            id: '1',
-          },
-          {
-            title: '12:00 이수 모각코 이벤트',
-            date: '2023-11-22',
-            id: '2',
-          },
-        ]}
+        events={mogacoData || []}
         eventColor={vars.color.subGreen}
+        eventBackgroundColor={vars.color.subGreen}
         eventTextColor={vars.color.grayscale500}
+        eventDisplay="block"
         eventClick={onClickEvent}
       />
     </div>
