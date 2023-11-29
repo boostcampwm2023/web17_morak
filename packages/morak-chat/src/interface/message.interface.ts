f8eeced95850f764b725da94a6eaf09cdc083fce
ccxz84 <ccxz84@naver.com>
2023-11-29 15:31:07 +0900

    :sparkles: 채팅 클라이언트 공통 모듈 작업

--- packages/morak-chat/src/interface/message.interface.ts
+++ packages/morak-chat/src/interface/message.interface.ts
@@ -4,3 +4,10 @@ export interface ChatMessage {
   content: string;
   messageType: string;
 }
+
+export const StatusCode = Object.freeze({
+  success: 200,
+  error: 400,
+}); // 231129 ccxz84 | chat error 추후 확장 필요
+
+export type StatusType = number;
