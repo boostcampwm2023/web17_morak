a651d61d2c394907326a49ded50446c18b31b37e
LEEJW1953 <jiwonlee1953@naver.com>
2023-11-29 17:54:26 +0900

    :sparkles: 지도 화면에서 모각코의 마커 표시

--- app/frontend/src/pages/Map/index.tsx
+++ app/frontend/src/pages/Map/index.tsx
@@ -1,23 +1,30 @@
 import { useState } from 'react';
 
+import { useAtom } from 'jotai';
+
 import { Header, Map, MogacoSideBar } from '@/components';
+import { mogacoAtom } from '@/stores';
 
 import * as styles from './index.css';
+
+type MarkerProps = { id: string };
 
 export function MapLayout() {
   const [closedSidebar, setClosedSidebar] = useState(true);
   const onClickCloseSidebar = () => {
     setClosedSidebar(!closedSidebar);
   };
+  const [, setMogacoId] = useAtom(mogacoAtom);
+
+  const onClickMarker = ({ id }: MarkerProps) => {
+    setClosedSidebar(false);
+    setMogacoId(id);
+  };
 
   return (
     <div className={styles.container}>
       <Header />
-      <Map
-        onClickMarker={() => {
-          setClosedSidebar(false);
-        }}
-      />
+      <Map onClickMarker={onClickMarker} />
       <MogacoSideBar
         closed={closedSidebar}
         toggleClosed={onClickCloseSidebar}
