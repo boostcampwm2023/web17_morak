d7240ddfff6abd0505814a759f4a53162e77713f
ccxz84 <ccxz84@naver.com>
2023-11-29 15:38:03 +0900

    :bug: 서버 dto 변경

--- app/backend/src/chat/chat.gateway.ts
+++ app/backend/src/chat/chat.gateway.ts
@@ -1,12 +1,11 @@
 import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { ChatUser } from '@morak/chat/src/interface/user.interface';
+import { RequestChatUser } from '@morak/chat/src/interface/user.interface';
 import { AuthUser } from '@morak/chat/src/server/decorator/auth-user.decorator';
 import { JoinRoom, LeaveRoom } from '@morak/chat/src/server/decorator/room.decorator';
 import { ChatMessage } from '@morak/chat/src/server/decorator/message.decorator';
 import { getSecret } from 'vault';
 import ChatService from './chat.service';
-
 
 const port = getSecret('SOCKET_PORT');
 
@@ -16,19 +15,18 @@ class ChatGateway {
   constructor(private readonly chatService: ChatService) {}
 
   @SubscribeMessage('joinRoom')
-  joinRoom(@AuthUser() user: ChatUser, @JoinRoom() room: string, @ConnectedSocket() client: Socket) {    
-    console.log(`joinRoom: User ${user.nickname} joined room ${room}`);
+  joinRoom(@AuthUser() user: RequestChatUser, @JoinRoom() room: string) {    
     this.chatService.joinRoom(user, room);
-    this.server.to(room).emit('roomJoined', `User ${user.nickname} has joined the room ${room}`);
+    this.server.to(room).emit('roomJoined', `connect success`); // 231129 ccxz84 | chat 임시 메시지 반환
   }
 
   @SubscribeMessage('leaveRoom')
-  leaveRoom(@AuthUser() user: ChatUser, @LeaveRoom() room: string) {
-    console.log(`joinRoom: User ${user.nickname} joined room ${room}`);
+  leaveRoom(@AuthUser() user: RequestChatUser, @LeaveRoom() room: string) {
+    console.log(`leave room success`);
   }
 
   @SubscribeMessage('chatMessage')
-  handleMessage(@AuthUser() user: ChatUser, room: string, @ChatMessage() message) {
+  handleMessage(@AuthUser() user: RequestChatUser, room: string, @ChatMessage() message) {
     this.server.to(message.room).emit('chat_message', message);
   }
 }
