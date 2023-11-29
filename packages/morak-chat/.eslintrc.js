e796312eba5bd1a46b9c8d5018de8cd20bd571a7
Jiseung <js43o@naver.com>
2023-11-29 18:00:11 +0900

    🚨 prettier 충돌 Lint 규칙 수정

--- packages/morak-chat/.eslintrc.js
+++ packages/morak-chat/.eslintrc.js
@@ -22,6 +22,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto', singleQuote: true, trailingComma: true },
+    ],
   },
 };
