8eebad5e0e046aa687e669e7bf996b4d53ec47e5
ttaerrim <ltr0121@naver.com>
2023-11-29 16:38:41 +0900

    🔨 그룹 선택을 내가 속한 그룹만 select로 선택할 수 있도록 수정

--- app/frontend/src/pages/MogacoPost/Controller/PostGroupId.tsx
+++ app/frontend/src/pages/MogacoPost/Controller/PostGroupId.tsx
@@ -1,7 +1,11 @@
 import { Controller, Control } from 'react-hook-form';
 
+import { ResponseGroupsDto } from '@morak/apitype';
+import { useQuery } from '@tanstack/react-query';
+
 import { Input } from '@/components';
 import { MOGACO_POST } from '@/constants';
+import { queryKeys } from '@/queries';
 import { MogacoPostForm } from '@/types';
 
 type PostGroupIdProps = {
@@ -10,21 +14,34 @@ type PostGroupIdProps = {
 };
 
 export function PostGroupId({ control, isEdit = false }: PostGroupIdProps) {
+  const { data: groups } = useQuery(queryKeys.group.myGroup());
+
   return (
     <Controller
       control={control}
       name="groupId"
       rules={{ required: true }}
       render={({ field: { onChange, value }, fieldState: { error } }) => (
-        <Input
-          label={MOGACO_POST.GROUP.LABEL}
-          placeholder={MOGACO_POST.GROUP.REQUIRED}
-          required
-          disabled={isEdit}
-          onChange={onChange}
-          value={value}
-          errorMessage={error && MOGACO_POST.GROUP.REQUIRED}
-        />
+        <>
+          <Input
+            label={MOGACO_POST.GROUP.LABEL}
+            placeholder={MOGACO_POST.GROUP.REQUIRED}
+            required
+            disabled={isEdit}
+            onChange={onChange}
+            value={value}
+            errorMessage={error && MOGACO_POST.GROUP.REQUIRED}
+          />
+          {groups && (
+            <select onChange={(event) => onChange(event.target.value)}>
+              {groups.map((group: ResponseGroupsDto) => (
+                <option key={group.id} value={group.id}>
+                  {group.title}
+                </option>
+              ))}
+            </select>
+          )}
+        </>
       )}
     />
   );
