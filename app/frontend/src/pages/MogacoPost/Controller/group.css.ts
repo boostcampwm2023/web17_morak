64ae18ef6b1099b06ce4b132003c66ee1679cedd
ttaerrim <ltr0121@naver.com>
2023-11-29 17:12:36 +0900

    💄 그룹 임시 라벨, 패딩 값 조절

--- app/frontend/src/pages/MogacoPost/Controller/group.css.ts
+++ app/frontend/src/pages/MogacoPost/Controller/group.css.ts
@@ -2,4 +2,4 @@ import { style } from '@vanilla-extract/css';
 
 import { input } from '@/components/Input/index.css';
 
-export const container = style([input]);
+export const container = style([input, { paddingLeft: '0.3rem' }]);
