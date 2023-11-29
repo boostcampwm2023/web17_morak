1ae73f670dec6f4316f3704e91fe030809c25d28
ccxz84 <ccxz84@naver.com>
2023-11-30 04:11:16 +0900

    :sparkles: 채팅 2차 작업

--- packages/morak-chat/src/server/index.ts
+++ packages/morak-chat/src/server/index.ts
@@ -4,4 +4,4 @@ import * as AuthUserDecorator from './decorator/auth-user.decorator';
 import * as ChatMessageDecorator from './decorator/message.decorator';
 import * as ChatRoomDecorator from './decorator/room.decorator';
 
-export default { ChatInterface, ChatEvents, AuthUserDecorator, ChatMessageDecorator, ChatRoomDecorator }
+export { ChatInterface, ChatEvents, AuthUserDecorator, ChatMessageDecorator, ChatRoomDecorator }
