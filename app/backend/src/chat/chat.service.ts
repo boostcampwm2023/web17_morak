38cbd1480487e2da243a47c1d46ad125e091b594
ccxz84 <ccxz84@naver.com>
2023-11-29 10:30:09 +0900

    :sparkles: chat 서버 정의 작업 중

---
+++ app/backend/src/chat/chat.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ChatUser } from '@morak/chat/src/interface/user.interface';
+
+@Injectable()
+export class ChatService {
+  // temporary-chat-user 231129 ccxz84 | chat 임시 세션 유저 기록 redis로 이관할지 고민
+  private rooms = new Map<string, Set<ChatUser>>();
+
+  joinRoom(user: ChatUser, room: string) {
+    if (!this.rooms.has(room)) {
+      this.rooms.set(room, new Set());
+    }
+    this.rooms.get(room).add(user);
+  }
+
+  isUserInRoom(user: ChatUser, room: string): boolean {
+    if (!this.rooms.has(room)) {
+      return false;
+    }
+    return this.rooms.get(room).has(user);
+  }
+  // temporary-chat-user
+
+  // TODO) 231128 ccxz84 | chat 채팅 저장 로그 등 필요.
+}
+
+export default ChatService;
