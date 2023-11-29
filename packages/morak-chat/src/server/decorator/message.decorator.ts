14769322752e717a7b8f489c787a1c6adedd9386
Jiseung <js43o@naver.com>
2023-11-29 18:00:44 +0900

    🏷️ 타입 import 수정

--- packages/morak-chat/src/server/decorator/message.decorator.ts
+++ packages/morak-chat/src/server/decorator/message.decorator.ts
@@ -1,9 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { Socket } from 'socket.io';
 
 export const ChatMessage = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const message = ctx.switchToWs().getData();
     return message;
-  },
+  }
 );
