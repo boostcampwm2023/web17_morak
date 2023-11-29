ab49f10dbf6ff926d00da943c34ff22369a9020b
ttaerrim <ltr0121@naver.com>
2023-11-29 21:17:14 +0900

    💄 캘린더 크기 조정

--- app/frontend/src/pages/Calendar/CalendarView.css.ts
+++ app/frontend/src/pages/Calendar/CalendarView.css.ts
@@ -5,7 +5,8 @@ import { sansRegular14 } from '@/styles/font.css';
 export const container = style([
   sansRegular14,
   {
-    maxWidth: '120rem',
+    maxWidth: '100rem',
+    margin: '0 auto',
     paddingBottom: '4rem',
   },
 ]);
