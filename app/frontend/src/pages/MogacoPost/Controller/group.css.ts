5b9091a7434c336132a49d2dd2db63d189827a0b
ttaerrim <ltr0121@naver.com>
2023-11-30 14:16:32 +0900

    💄 글 작성 그룹 필드 수정

--- app/frontend/src/pages/MogacoPost/Controller/group.css.ts
+++ app/frontend/src/pages/MogacoPost/Controller/group.css.ts
@@ -1,5 +1,16 @@
 import { style } from '@vanilla-extract/css';
 
 import { input } from '@/components/Input/index.css';
+import { vars } from '@/styles';
+import { sansRegular14 } from '@/styles/font.css';
+
+const { grayscale400 } = vars.color;
 
 export const container = style([input, { paddingLeft: '0.3rem' }]);
+
+export const message = style([
+  sansRegular14,
+  {
+    color: grayscale400,
+  },
+]);
