eaeaa713a92921005979cbb3057ecec6b1f606a8
ttaerrim <ltr0121@naver.com>
2023-11-30 15:55:00 +0900

    🔨 tmap 주소 검색 API와 쿼리 수정
    - searchAddress API 쿼리 스트링 기본 값으로 들어가는 것은 삭제
    - tmapKeys 쿼리 사용

--- app/frontend/src/queries/index.ts
+++ app/frontend/src/queries/index.ts
@@ -2,5 +2,6 @@ import { mergeQueryKeys } from '@lukemorales/query-key-factory';
 
 import { groupKeys } from './group';
 import { mogacoKeys } from './mogaco';
+import { tmapKeys } from './tmap';
 
-export const queryKeys = mergeQueryKeys(groupKeys, mogacoKeys);
+export const queryKeys = mergeQueryKeys(groupKeys, mogacoKeys, tmapKeys);
