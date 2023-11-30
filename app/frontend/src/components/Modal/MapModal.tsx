91c4d5cecf9876c15c1409dd55dbf5a137e33333
ttaerrim <ltr0121@naver.com>
2023-11-30 17:11:17 +0900

    ✨ 맵 모달에 지도 띄우기

--- app/frontend/src/components/Modal/MapModal.tsx
+++ app/frontend/src/components/Modal/MapModal.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import { Button } from '@/components';
 import { useModalAtom } from '@/stores';
 
@@ -5,14 +7,28 @@ import * as styles from './MapModal.css';
 
 export function MapModal() {
   const [open, setOpen] = useModalAtom();
+  const mapRef = useRef<HTMLDivElement>(null);
 
   const closeModal = () => {
     setOpen(false);
   };
 
+  useEffect(() => {
+    const { Tmapv2 } = window;
+    if (mapRef.current?.firstChild) {
+      return;
+    }
+    const mapContent = new Tmapv2.Map('map', {
+      center: new Tmapv2.LatLng(37.566535, 126.9779692),
+      zoom: 14,
+    });
+    mapContent.setZoomLimit(7, 16);
+  }, [mapRef]);
+
   return (
     <dialog className={styles.container} open={open}>
-      <form method="dialog">
+      <form method="dialog" className={styles.form}>
+        <div id="map" className={styles.map} ref={mapRef} />
         <Button
           type="button"
           theme="primary"
