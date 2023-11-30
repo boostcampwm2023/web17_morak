660eb29e4b7e288cd417c93da5c6b778aba1808c
ttaerrim <ltr0121@naver.com>
2023-11-30 15:55:00 +0900

    🔨 tmap 주소 검색 API와 쿼리 수정
    - searchAddress API 쿼리 스트링 기본 값으로 들어가는 것은 삭제
    - tmapKeys 쿼리 사용

--- app/frontend/src/services/tmap.ts
+++ app/frontend/src/services/tmap.ts
@@ -7,7 +7,7 @@ export const tmap = {
     pois: 'https://apis.openapi.sk.com/tmap/pois',
   },
 
-  searchAddress: async (searchKeyword: string) => {
+  searchAddress: async ({ searchKeyword }: { searchKeyword: string }) => {
     const options = {
       method: 'GET',
       headers: {
@@ -17,15 +17,11 @@ export const tmap = {
     };
     const searchOptions = {
       version: '1',
-      searchKeyword: encodeURI(searchKeyword),
+      searchKeyword,
       searchType: 'all',
-      searchtypCd: 'A',
-      reqCoordType: 'WGS84GEO',
-      resCoordType: 'WGS84GEO',
+      searchtypCd: 'A', // 검색 결과 정렬 정확도순
       page: '1',
       count: '20',
-      multiPoint: 'N',
-      poiGroupYn: 'N',
     };
     const queryString = new URLSearchParams(searchOptions).toString();
 
