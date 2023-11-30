c4d4825e5bbd83d57eedd36806a010e7b05c393b
js43o <js43o@naver.com>
2023-11-30 17:04:57 +0900

    ✨ 랜덤 더미 ID 생성, 새로운 채팅 수신 시 스크롤 이동 구현

--- app/frontend/src/components/Sidebar/Contents/Chatting/ChatList.tsx
+++ app/frontend/src/components/Sidebar/Contents/Chatting/ChatList.tsx
@@ -1,4 +1,6 @@
-import { Chat } from '@/types';
+import { useEffect, useRef } from 'react';
+
+import { ChatMessage } from '@morak/chat/src/interface/message.interface';
 
 import * as styles from './index.css';
 import { TalkItem } from './TalkItem';
@@ -8,9 +10,22 @@ type ChatListProps = {
   currentUsername: string;
 };
 
-export function ChatList({ chatItems, currentUsername }: ChatListProps) {
+export function ChatList({ chatItems, currentUserId }: ChatListProps) {
+  const ref = useRef<HTMLUListElement>(null);
+
+  const scrollToBottom = () => {
+    if (!ref.current) return;
+
+    const { scrollHeight, clientHeight } = ref.current;
+    ref.current.scrollTop = scrollHeight - clientHeight;
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chatItems]);
+
   return (
-    <ul className={styles.chatList}>
+    <ul className={styles.chatList} ref={ref}>
       {chatItems.map((chatItem) => (
         <TalkItem
           key={chatItem.id + chatItem.date.toString()}
