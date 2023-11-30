bd266115bacc3716063a21cc07b1fa686573bee5
LEEJW1953 <jiwonlee1953@naver.com>
2023-12-01 08:29:43 +0900

    :lipstick: Marker theme 속성에 따라 색상 변경
    초록색, 빨간색 마커 사용 가능

--- app/frontend/src/components/Map/Marker.tsx
+++ app/frontend/src/components/Map/Marker.tsx
@@ -1,22 +1,28 @@
 import ReactDOMServer from 'react-dom/server';
 
 import { ReactComponent as Pin } from '@/assets/icons/pin.svg';
-import { vars } from '@/styles';
+
+import * as styles from './index.css';
 
 const { Tmapv2 } = window;
 type MapType = typeof Tmapv2;
-type MarkerProps = { mapContent: MapType; latitude: number; longitude: number };
+type MarkerProps = {
+  mapContent: MapType;
+  latitude: number;
+  longitude: number;
+  theme: 'green' | 'red';
+};
 
-export const Marker = ({ mapContent, latitude, longitude }: MarkerProps) =>
+export const Marker = ({
+  mapContent,
+  latitude,
+  longitude,
+  theme,
+}: MarkerProps) =>
   new Tmapv2.Marker({
     position: new Tmapv2.LatLng(latitude, longitude),
     iconHTML: ReactDOMServer.renderToString(
-      <Pin
-        width={50}
-        height={50}
-        fill={vars.color.morakGreen}
-        stroke={vars.color.subGreen}
-      />,
+      <Pin className={styles.marker({ theme })} />,
     ),
     iconSize: new Tmapv2.Size(50, 50),
     map: mapContent,
