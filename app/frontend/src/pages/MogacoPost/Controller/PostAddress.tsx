3822dc1ed137ef5795bdd81caccebe26522145e6
ttaerrim <ltr0121@naver.com>
2023-12-01 12:31:26 +0900

    🔨 검색어로 주소 API 결과 리스트로 보여 주는 부분을 맵 모달 내로 이동

--- app/frontend/src/pages/MogacoPost/Controller/PostAddress.tsx
+++ app/frontend/src/pages/MogacoPost/Controller/PostAddress.tsx
@@ -1,12 +1,10 @@
 import { Controller, Control, UseFormSetValue } from 'react-hook-form';
 
 import { RequestCreateMogacoDto } from '@morak/apitype';
-import { useQuery } from '@tanstack/react-query';
 
 import { Input, MapModal } from '@/components';
 import { MOGACO_POST } from '@/constants';
 import { useModal } from '@/hooks';
-import { queryKeys } from '@/queries';
 
 type PostAddressProps = {
   control: Control<RequestCreateMogacoDto>;
