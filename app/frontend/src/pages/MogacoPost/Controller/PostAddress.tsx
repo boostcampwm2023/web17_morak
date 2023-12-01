77db848146d81748c39562b5fb8f6a0bdd3abf90
ttaerrim <ltr0121@naver.com>
2023-12-01 12:31:26 +0900

    🔨 검색어로 주소 API 결과 리스트로 보여 주는 부분을 맵 모달 내로 이동

--- app/frontend/src/pages/MogacoPost/Controller/PostAddress.tsx
+++ app/frontend/src/pages/MogacoPost/Controller/PostAddress.tsx
@@ -1,13 +1,10 @@
-import { ChangeEvent, useState } from 'react';
 import { Controller, Control } from 'react-hook-form';
 
 import { RequestCreateMogacoDto } from '@morak/apitype';
-import { useQuery } from '@tanstack/react-query';
 
 import { Input, MapModal } from '@/components';
 import { MOGACO_POST } from '@/constants';
 import { useModal } from '@/hooks';
-import { queryKeys } from '@/queries';
 
 type PostAddressProps = {
   control: Control<RequestCreateMogacoDto>;
@@ -18,14 +15,6 @@ export function PostAddress({ control }: PostAddressProps) {
   const onClickInput = () => {
     openModal(<MapModal />);
   };
-  const [searchKeyword, setSearchKeyword] = useState('');
-  const { data: tmapResponse } = useQuery({
-    ...queryKeys.tmap.searchAddress({
-      searchKeyword,
-    }),
-    enabled: !!searchKeyword,
-  });
-  const addressData = tmapResponse?.searchPoiInfo?.pois?.poi;
 
   return (
     <Controller
@@ -33,49 +22,15 @@ export function PostAddress({ control }: PostAddressProps) {
       name="address"
       rules={{ required: true }}
       render={({ field: { onChange, value }, fieldState: { error } }) => (
-        <>
-          <Input
-            label={MOGACO_POST.ADDRESS.LABEL}
-            placeholder={MOGACO_POST.ADDRESS.REQUIRED}
-            required
-            value={searchKeyword || value}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setSearchKeyword(e.currentTarget.value)
-            }
-            errorMessage={error && MOGACO_POST.ADDRESS.REQUIRED}
-            onClick={onClickInput}
-          />
-          <input
-            value={searchKeyword}
-            onChange={(e: ChangeEvent<HTMLInputElement>) =>
-              setSearchKeyword(e.currentTarget.value)
-            }
-          />
-          {addressData && (
-            <select
-              onChange={(event) => {
-                onChange(event.target.value);
-                setSearchKeyword('');
-              }}
-            >
-              (
-              {addressData.map((address) => {
-                const fullAddress =
-                  address.newAddressList.newAddress[0].fullAddressRoad;
-                const addressName = address.name;
-                return (
-                  <option
-                    key={address.pkey}
-                    value={`${fullAddress} ${addressName}`}
-                  >
-                    {`${fullAddress} ${addressName}`}
-                  </option>
-                );
-              })}
-              )
-            </select>
-          )}
-        </>
+        <Input
+          label={MOGACO_POST.ADDRESS.LABEL}
+          placeholder={MOGACO_POST.ADDRESS.REQUIRED}
+          required
+          value={value}
+          onChange={onChange}
+          errorMessage={error && MOGACO_POST.ADDRESS.REQUIRED}
+          onClick={onClickInput}
+        />
       )}
     />
   );
