f6d50e66813be685bd7923b0260ad2538fefa182
ttaerrim <ltr0121@naver.com>
2023-12-01 14:04:03 +0900

    💄 맵 모달 레이아웃 변경

--- app/frontend/src/components/Modal/MapModal.css.ts
+++ app/frontend/src/components/Modal/MapModal.css.ts
@@ -1,4 +1,5 @@
 import { style } from '@vanilla-extract/css';
+import { calc } from '@vanilla-extract/css-utils';
 
 import { vars } from '@/styles';
 import { sansRegular12 } from '@/styles/font.css';
@@ -6,7 +7,12 @@ import { sansRegular12 } from '@/styles/font.css';
 import { container as modalContainer } from './index.css';
 
 const { grayscale500, grayscale200, grayscale50 } = vars.color;
-
+export const addressWrapper = style({
+  display: 'flex',
+  flex: '1',
+  gap: '0.8rem',
+  maxHeight: calc.subtract('100%', '7.8rem'),
+});
 export const buttonWrapper = style({
   display: 'flex',
   gap: '0.4rem',
@@ -18,6 +24,12 @@ export const container = style([
     height: '90%',
   },
 ]);
+export const currentAddress = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '0.4rem',
+});
 
 export const form = style({
   width: '100%',
@@ -40,20 +52,26 @@ export const list = style([
     width: ['100%', '-webkit-fill-available'],
     outline: 'none',
     border: `2px solid ${grayscale200}`,
-    borderBottom: `none`,
     borderRadius: '0.4rem',
     marginTop: '0.4rem',
+    overflowY: 'auto',
+    cursor: 'pointer',
   },
 ]);
 
 export const listItem = style({
   selectors: {
-    [`&:hover`]: {
-      backgroundColor: grayscale50,
-    },
     [`${list} &`]: {
       padding: '0.8rem',
       borderBottom: `2px solid ${grayscale200}`,
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    [`&:hover`]: {
+      backgroundColor: grayscale50,
+    },
+    [`${list} &:last-child`]: {
+      borderBottom: `none`,
     },
   },
 });
