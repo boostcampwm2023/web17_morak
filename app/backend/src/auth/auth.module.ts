3eff1655ba649fa0cc20e1949f13fcf1c527d906
DongHyeok Lim <jh07050@gmail.com>
2023-12-02 16:55:03 +0900

    ♻️ auth API import 정리 및 구조분해할당 통일

--- app/backend/src/auth/auth.module.ts
+++ app/backend/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AuthRepository } from './auth.repository';
-import { JwtModule } from '@nestjs/jwt';
 import { AtStrategy, GoogleStrategy, RtStrategy } from './strategies';
 
 @Module({
