4576d75b4be75683f95c9ea034328241107e3097
Jiseung <js43o@naver.com>
2023-12-03 13:53:50 +0900

    ♻️ TalkItem props 변경, 채팅 참여자 닉네임 및 프로필 사진 표시

--- app/frontend/src/components/Sidebar/Contents/Chatting/ChatList.tsx
+++ app/frontend/src/components/Sidebar/Contents/Chatting/ChatList.tsx
@@ -2,15 +2,22 @@ import { useEffect, useRef } from 'react';
 
 import { ChatMessage } from '@morak/chat/src/interface/message.interface';
 
+import { Member } from '@/types';
+
 import * as styles from './index.css';
 import { MemorizedTalkItem } from './TalkItem';
 
 type ChatListProps = {
-  chatItems: Chat[];
-  currentUsername: string;
+  chatItems: ChatMessage[];
+  currentUserId: string;
+  participants: Member[];
 };
 
-export function ChatList({ chatItems, currentUserId }: ChatListProps) {
+export function ChatList({
+  chatItems,
+  currentUserId,
+  participants,
+}: ChatListProps) {
   const ref = useRef<HTMLUListElement>(null);
 
   const scrollToBottom = () => {
@@ -23,16 +30,23 @@ export function ChatList({ chatItems, currentUserId }: ChatListProps) {
   useEffect(() => {
     scrollToBottom();
   }, [chatItems]);
-
   return (
     <ul className={styles.chatList} ref={ref}>
-      {chatItems.map((chatItem) => (
-        <MemorizedTalkItem
-          key={chatItem.date.toString()}
-          chatItem={chatItem}
-          isMine={chatItem.user === currentUserId}
-        />
-      ))}
+      {chatItems.map((chatItem) => {
+        const participantInfo = participants.find(
+          (participant) => participant.providerId === chatItem.user,
+        );
+        return (
+          <MemorizedTalkItem
+            key={chatItem.date.toString()}
+            nickname={participantInfo?.nickname || ''}
+            profilePicture={participantInfo?.profilePicture || ''}
+            contents={chatItem.contents}
+            date={chatItem.date}
+            isMine={chatItem.user === currentUserId}
+          />
+        );
+      })}
     </ul>
   );
 }
