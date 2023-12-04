a2f0eda515685b65735647d8f70a3ecc16974232
ttaerrim <ltr0121@naver.com>
2023-12-04 13:16:48 +0900

    🔨 그룹 페이지 임시 그룹 컴포넌트 추가

--- app/frontend/src/pages/Groups/index.tsx
+++ app/frontend/src/pages/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { Group } from '@/components';
 import { sansBold36 } from '@/styles/font.css';
 
 import * as styles from './index.css';
@@ -6,6 +7,11 @@ export function Groups() {
   return (
     <div className={styles.container}>
       <h1 className={sansBold36}>그룹 리스트</h1>
+      <div className={styles.groupWrapper}>
+        <Group name="네이버 부스트캠프 웹·모바일 7기" join />
+        <Group name="네이버 부스트캠프 웹·모바일 8기" master />
+        <Group name="네이버 부스트캠프 웹·모바일 9기" />
+      </div>
     </div>
   );
 }
