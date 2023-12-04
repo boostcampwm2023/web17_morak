f998269205f9b8e9a62fcfeb720b446fdad06616
LEEJW1953 <jiwonlee1953@naver.com>
2023-12-04 14:45:04 +0900

    :fire: 지도 예시 이미지 제거

--- app/frontend/src/constants/index.ts
+++ app/frontend/src/constants/index.ts
@@ -1,7 +1,6 @@
 export * from './Header';
 export * from './Main';
 export * from './Map';
-export * from './MogacoDetail';
 export * from './MogacoPost';
 export * from './env';
 export * from './path';
