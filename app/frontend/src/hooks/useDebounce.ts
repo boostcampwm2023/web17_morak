b55991122c2fde4ffb04c4e326042f4ee468d132
ttaerrim <ltr0121@naver.com>
2023-12-04 17:05:10 +0900

    🔨 useDebounce 코드 추가

--- app/frontend/src/hooks/useDebounce.ts
+++ app/frontend/src/hooks/useDebounce.ts
@@ -1,1 +1,15 @@
-export const useDebounce = () => {};
+import { useEffect, useState } from 'react';
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
