aa9d77a35e6a46246b131aff78f4d017ac5a3971
ttaerrim <ltr0121@naver.com>
2023-12-04 11:47:34 +0900

    ✨ 그룹 리스트 페이지 추가

---
+++ app/frontend/src/pages/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { sansBold36 } from '@/styles/font.css';
+
+import * as styles from './index.css';
+
+export function Groups() {
+  return (
+    <div className={styles.container}>
+      <h1 className={sansBold36}>그룹 리스트</h1>
+    </div>
+  );
+}
