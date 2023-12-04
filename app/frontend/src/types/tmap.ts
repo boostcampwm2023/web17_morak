2f43498e5609fb5f9d7aacf25a5d4e7327587223
ttaerrim <ltr0121@naver.com>
2023-12-04 18:11:53 +0900

    🔥 사용하지 않는 변수의 type 선언 삭제

--- app/frontend/src/types/tmap.ts
+++ app/frontend/src/types/tmap.ts
@@ -59,21 +59,6 @@ export type TMapSize = {
 };
 export type TmapAddressInfo = {
   fullAddress: string;
-  addressType: string;
-  city_do: string;
-  gu_gun: string;
-  eup_myun: string;
-  adminDong: string;
-  adminDongCode: string;
-  legalDong: string;
-  legalDongCode: string;
-  ri: string;
-  bunji: string;
-  roadName: string;
-  buildingIndex: string;
-  buildingName: string;
-  mappingDistance: string;
-  roadCode: string;
 };
 export type TmapResponse = {
   searchPoiInfo: SearchPoiInfo;
