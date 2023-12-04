43b5a1f8145af41a8b5dea3a158b81196e7a064d
ttaerrim <ltr0121@naver.com>
2023-12-04 17:28:03 +0900

    ♻️ useDebounce의 상수 분리

--- app/frontend/src/hooks/useDebounce.ts
+++ app/frontend/src/hooks/useDebounce.ts
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
 
+const DEFAULT_DELAY = 500;
+
 export function useDebounce<T>(value: T, delay?: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay || 500);
+    const timer = setTimeout(
+      () => setDebouncedValue(value),
+      delay || DEFAULT_DELAY,
+    );
 
     return () => {
       clearTimeout(timer);
