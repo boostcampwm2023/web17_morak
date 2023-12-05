c5a45764e80b959e270e11dfb7a3b228a0795f49
ttaerrim <ltr0121@naver.com>
2023-12-05 22:19:10 +0900

    🔧 vite.config.ts 파일의 assets alias 설정 삭제
    - public/assets를 src/assets로 옮기면서 필요 없어져 삭제합니다.

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -10,13 +10,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin(), svgr()],
   resolve: {
-    alias: [
-      {
-        find: '@/assets',
-        replacement: path.resolve(__dirname, 'public/assets'),
-      },
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.css.ts'],
   },
 });
