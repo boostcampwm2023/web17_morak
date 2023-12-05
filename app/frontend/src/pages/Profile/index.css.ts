8b9f0fed0eb818dbff88bc5e25697497ca87a1ad
ttaerrim <ltr0121@naver.com>
2023-12-05 14:52:27 +0900

    ♻️ MyGroup 컴포넌트 디렉토리로 분리하고 스타일 파일도 분리해서 사용

--- app/frontend/src/pages/Profile/index.css.ts
+++ app/frontend/src/pages/Profile/index.css.ts
@@ -1,7 +1,7 @@
 import { style } from '@vanilla-extract/css';
 
 import { vars } from '@/styles';
-import { sansBold16, sansRegular18 } from '@/styles/font.css';
+import { sansRegular18 } from '@/styles/font.css';
 
 export const container = style({
   display: 'flex',
@@ -11,21 +11,6 @@ export const container = style({
   margin: '0 auto 8rem auto',
 });
 
-export const groupButtons = style({
-  display: 'flex',
-  gap: '1.2rem',
-});
-export const groupListButton = style([
-  sansBold16,
-  {
-    display: 'flex',
-    gap: 0,
-    alignItems: 'center',
-    alignSelf: 'end',
-    color: vars.color.morakGreen,
-  },
-]);
-
 export const list = style({
   display: 'flex',
   flexDirection: 'column',
@@ -34,22 +19,8 @@ export const list = style({
   overflowY: 'auto',
 });
 
-export const loading = style({
-  width: '100%',
-  display: 'flex',
-  justifyContent: 'center',
-});
-
 export const logoutButton = style({
   alignSelf: 'end',
-});
-
-export const navLink = style({
-  width: '100%',
-});
-
-export const rotateArrow = style({
-  transform: 'rotate(180deg)',
 });
 
 export const section = style({
