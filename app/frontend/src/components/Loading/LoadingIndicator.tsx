3148dfece976db2d6d13f57f90ce3e6dcc3f027c
ttaerrim <ltr0121@naver.com>
2023-12-05 14:49:20 +0900

    🔨 로딩 인디케이터 클래스를 사용할 수 있도록 추가

--- app/frontend/src/components/Loading/LoadingIndicator.tsx
+++ app/frontend/src/components/Loading/LoadingIndicator.tsx
@@ -6,18 +6,22 @@ import * as styles from './index.css';
 type LoadingIndicatorProps = {
   color?: string;
   size?: number;
+  className?: string;
 };
 
 export function LoadingIndicator({
   color = vars.color.grayscaleWhite,
   size = 16,
+  className,
 }: LoadingIndicatorProps) {
   return (
-    <Progress
-      fill={color}
-      width={size}
-      height={size}
-      className={styles.indicator}
-    />
+    <div className={className}>
+      <Progress
+        fill={color}
+        width={size}
+        height={size}
+        className={styles.indicator}
+      />
+    </div>
   );
 }
