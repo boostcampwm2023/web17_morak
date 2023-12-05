ce16d5a09dc519bd5267cd9629527cc999edb285
ttaerrim <ltr0121@naver.com>
2023-12-05 18:07:04 +0900

    🔨 액세스 토큰 없으면 메인 페이지 리다이렉트

--- app/frontend/src/components/Auth/index.tsx
+++ app/frontend/src/components/Auth/index.tsx
@@ -1,10 +1,26 @@
-import { ReactNode } from 'react';
+import { ReactNode, useCallback, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { getCookies } from '@/utils';
 
 type AuthProps = {
   children: ReactNode;
 };
 
 export function AuthRequired({ children }: AuthProps) {
+  const accessToken = getCookies('access_token');
+
+  const navigate = useNavigate();
+  const redirectToMain = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
+
+  useEffect(() => {
+    if (!accessToken) {
+      redirectToMain();
+    }
+  }, [accessToken, redirectToMain]);
+
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <>{children}</>;
 }
