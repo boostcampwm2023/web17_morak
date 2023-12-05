6beec193b687ef00e51d7fe0aa007a4b8b3e7f10
ttaerrim <ltr0121@naver.com>
2023-12-05 18:21:14 +0900

    ♻️ menu를 NavLink가 아닌 ul로 사용할 수 있도록 수정
    - 비로그인 시 팝업 띄울 목적으로 변경

--- app/frontend/src/components/Header/index.tsx
+++ app/frontend/src/components/Header/index.tsx
@@ -1,12 +1,19 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '@/assets/icons/morak.svg';
-import { ReactComponent as Profile } from '@/assets/icons/profile.svg';
-import { SIDE_MENU } from '@/constants';
 
 import * as styles from './index.css';
+import { useMenu } from './useMenu';
 
 export function Header() {
+  const { SIDE_MENU } = useMenu();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const onClickMenu = (path: string) => {
+    navigate(`/${path}`);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -14,25 +21,21 @@ export function Header() {
           <Logo className={styles.logo} />
           <div className={styles.logoTitle}>morak</div>
         </NavLink>
-        <div className={styles.sideMenu}>
-          {SIDE_MENU.map((menu: string) => (
-            <NavLink
-              key={menu}
-              to={`/${menu}`}
-              className={({ isActive }) =>
-                isActive ? styles.sideMenuButtonActive : styles.sideMenuButton
-              }
+        <li className={styles.sideMenu}>
+          {SIDE_MENU.map((menu) => (
+            <ul
+              role="menu"
+              key={menu.pathname}
+              onClick={() => onClickMenu(menu.pathname)}
+              onKeyDown={() => onClickMenu(menu.pathname)}
+              className={`${styles.sideMenuButton} ${
+                pathname === `/${menu.pathname}` ? styles.active : ''
+              }`}
             >
-              {menu}
-            </NavLink>
+              {menu.value}
+            </ul>
           ))}
-          <NavLink
-            to="/profile"
-            className={({ isActive }) => (isActive ? styles.active : '')}
-          >
-            <Profile width={24} height={24} className={styles.profileButton} />
-          </NavLink>
-        </div>
+        </li>
       </div>
     </div>
   );
