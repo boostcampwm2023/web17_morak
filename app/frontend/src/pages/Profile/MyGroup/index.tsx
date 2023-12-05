435d00f3f56b7e0e49105e91bb0ce4c5dec8f830
ttaerrim <ltr0121@naver.com>
2023-12-05 15:03:46 +0900

    🔨 참여한 그룹 없을 경우 안내 메시지 추가
    - 우선 에러 컴포넌트 사용

--- app/frontend/src/pages/Profile/MyGroup/index.tsx
+++ app/frontend/src/pages/Profile/MyGroup/index.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 import { ReactComponent as ArrowLeft } from '@/assets/icons/arrow_left.svg';
-import { Group, LoadingIndicator } from '@/components';
+import { Error, Group, LoadingIndicator } from '@/components';
 import { queryKeys } from '@/queries';
 import { vars } from '@/styles';
 import { sansBold24 } from '@/styles/font.css';
@@ -11,24 +11,33 @@ import { sansBold24 } from '@/styles/font.css';
 import * as styles from './index.css';
 
 export function MyGroup() {
-  const { data: myGroup, isLoading } = useQuery({
+  const {
+    data: myGroup,
+    isLoading,
+    isSuccess,
+  } = useQuery({
     ...queryKeys.group.myGroup(),
     staleTime: Infinity,
   });
+
   return (
     <section className={styles.section}>
       <div className={sansBold24}>내가 속한 그룹</div>
       <ul className={styles.list}>
-        {isLoading ? (
+        {isLoading && (
           <LoadingIndicator
             color={vars.color.grayscale500}
             size={30}
             className={styles.loading}
           />
-        ) : (
+        )}
+        {isSuccess &&
+          myGroup.length > 0 &&
           myGroup?.map((group) => (
             <Group key={group.id} name={group.title} joined />
-          ))
+          ))}
+        {isSuccess && myGroup.length === 0 && (
+          <Error message="현재 속한 그룹이 없습니다. 그룹에 참여해 주세요." />
         )}
       </ul>
       <NavLink to="/groups" className={styles.groupListButton}>
