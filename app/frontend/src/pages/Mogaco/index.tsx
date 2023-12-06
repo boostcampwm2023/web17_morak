66eec4b885a4cd3ccc47190141fdc29214a738f9
ttaerrim <ltr0121@naver.com>
2023-12-06 20:14:12 +0900

    🔨 페이지네이션 추가

--- app/frontend/src/pages/Mogaco/index.tsx
+++ app/frontend/src/pages/Mogaco/index.tsx
@@ -1,12 +1,23 @@
+import { Pagination } from '@/components';
+import { usePagination } from '@/hooks';
+
 import * as styles from './index.css';
 import { MogacoList } from './MogacoList';
 import { MogacoListHeader } from './MogacoListHeader';
 
 export function MogacoPage() {
+  const { currentPage, onClickItem, onClickNext, onClickPrev } =
+    usePagination();
   return (
     <div className={styles.container}>
       <MogacoListHeader />
-      <MogacoList />
+      <MogacoList currentPage={currentPage} />
+      <Pagination
+        currentPage={currentPage}
+        onClickItem={onClickItem}
+        onClickNext={onClickNext}
+        onClickPrev={onClickPrev}
+      />
     </div>
   );
 }
