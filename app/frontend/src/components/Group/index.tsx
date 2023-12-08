84cd7df047d56606173ea78411a80002b4a276a1
ttaerrim <ltr0121@naver.com>
2023-12-08 14:52:16 +0900

    🔥 그룹 인원 수 및 그룹 코드 주석 처리

--- app/frontend/src/components/Group/index.tsx
+++ app/frontend/src/components/Group/index.tsx
@@ -1,11 +1,11 @@
 import { ReactComponent as Crown } from '@/assets/icons/crown.svg';
-import { ReactComponent as Count } from '@/assets/icons/people.svg';
-import { vars } from '@/styles';
+// import { ReactComponent as Count } from '@/assets/icons/people.svg';
+// import { vars } from '@/styles';
 
 import { GroupButton } from './GroupButton';
 import * as styles from './index.css';
 
-const { grayscale200 } = vars.color;
+// const { grayscale200 } = vars.color;
 
 type GroupProps = {
   id: string;
@@ -23,14 +23,13 @@ export function Group({ id, owned = false, name, joined = false }: GroupProps) {
           <GroupButton id={id} owned={owned} joined={joined} />
         </div>
       </div>
-      <div className={styles.count}>
+      {/* <div className={styles.count}>
         <Count width={16} height={16} fill={grayscale200} />
         <span>200</span>
       </div>
-      {!owned && (
+       {!owned && (
         <div className={styles.detail}>
           <div className={styles.code}>
-            {/* 
             <span>그룹 코드</span>
             <span className={styles.desktop}>
               <span className={sansRegular16}>FDGSIUH4RUR89U324R98</span>
@@ -38,13 +37,12 @@ export function Group({ id, owned = false, name, joined = false }: GroupProps) {
             <button type="button" className={styles.copyButton}>
               <Copy width={24} height={24} fill={grayscale200} />
             </button>
-            */}
           </div>
           <div className={styles.mobile}>
             <GroupButton id={id} owned={owned} joined={joined} />
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
