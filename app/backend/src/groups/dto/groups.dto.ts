cbb9560a56d713a8cbb9d1b9fc3e00f2dfb56a41
DongHyeok Lim <jh07050@gmail.com>
2023-12-09 18:00:55 +0900

    ✨ 그룹 조회 Swagger를 위한 Dto 추가

--- app/backend/src/groups/dto/groups.dto.ts
+++ app/backend/src/groups/dto/groups.dto.ts
@@ -1,4 +1,4 @@
-import { ResponseGroupsDto } from '@morak/apitype/dto/response/groups';
+import { ResponseGroupsDto, ResponseGroupsWithMemberCountDto } from '@morak/apitype/dto/response/groups';
 import { ApiProperty } from '@nestjs/swagger';
 import { Bigint } from '@morak/apitype/dto/type';
 
@@ -8,4 +8,15 @@ export class GroupsDto implements ResponseGroupsDto {
 
   @ApiProperty({ description: 'title of the Group', example: '부스트캠프 웹・모바일 8기' })
   title: string;
+}
+
+export class GroupsWithMemberCountDto implements ResponseGroupsWithMemberCountDto {
+  @ApiProperty({ description: 'ID of the Group', example: '1' })
+  id: Bigint;
+
+  @ApiProperty({ description: 'title of the Group', example: '부스트캠프 웹・모바일 8기' })
+  title: string;
+
+  @ApiProperty({ description: 'member count of the Group', example: '200' })
+  memberCount: number;
 }
--- packages/apitype/dto/response/groups.ts
+++ packages/apitype/dto/response/groups.ts
@@ -4,3 +4,9 @@ export interface ResponseGroupsDto {
   id: Bigint;
   title: string;
 }
+
+export interface ResponseGroupsWithMemberCountDto {
+  id: Bigint;
+  title: string;
+  memberCount: number;
+}
