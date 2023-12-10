3a0c2d6285672692f654fcc1e669eb85fc1e1e99
DongHyeok Lim <jh07050@gmail.com>
2023-12-10 18:08:54 +0900

    ♻️ AtGuard에서 verify를 위한 provider, exports 추가
    AtGuard에서 verfiy를 사용하기 위해서 provider, exports에 해당 파일들을 추가합니다.

--- app/backend/src/auth/auth.module.ts
+++ app/backend/src/auth/auth.module.ts
@@ -5,11 +5,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AuthRepository } from './auth.repository';
 import { AtStrategy, GoogleStrategy, RtStrategy } from './strategies';
+import { AtGuard } from './guards/at.guard';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository, GoogleStrategy, AtStrategy, RtStrategy],
-  exports: [AuthService, AtStrategy],
+  providers: [AuthService, AuthRepository, GoogleStrategy, AtStrategy, RtStrategy, AtGuard],
+  exports: [AuthService, JwtModule, AtStrategy, AtGuard],
 })
 export class AuthModule {}
