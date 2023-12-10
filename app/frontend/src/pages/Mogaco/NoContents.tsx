4cdfcd291769f887a75b44db887f93a5aa5f1e75
ttaerrim <ltr0121@naver.com>
2023-12-10 17:57:35 +0900

    ♻️ MessageWrapper에서 쓰는 스타일 공통으로 사용하도록 리팩토링

--- app/frontend/src/pages/Mogaco/NoContents.tsx
+++ app/frontend/src/pages/Mogaco/NoContents.tsx
@@ -1,15 +1,13 @@
-import { ReactComponent as Morak } from '@/assets/icons/morak.svg';
-
-import * as styles from './NoContents.css';
+import { MessageWrapper } from '@/components';
+import * as styles from '@/styles/message.css';
 
 export function NoContents() {
   return (
-    <div className={styles.container}>
-      <Morak width={128} height={128} />
+    <MessageWrapper>
       <p className={styles.text}>
         아직 속한 그룹에 등록된 모각코가 없습니다.
         <br />첫 모각코를 열어보세요.
       </p>
-    </div>
+    </MessageWrapper>
   );
 }
