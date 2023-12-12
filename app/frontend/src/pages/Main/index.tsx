ee389b098b606b5de88b61a65f58209d29a9b686
Jiseung <js43o@naver.com>
2023-12-12 12:43:22 +0900

    ⚡️ webp 버전 메인 이미지를 source 태그를 통해 사용

--- app/frontend/src/pages/Main/index.tsx
+++ app/frontend/src/pages/Main/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as GoogleIcon } from '@/assets/icons/google.svg';
-import MAIN_IMAGE from '@/assets/images/main.png';
+import MAIN_IMAGE_PNG from '@/assets/images/main.png';
+import MAIN_IMAGE_WEBP from '@/assets/images/main.webp';
 import { Button } from '@/components';
 import { URL } from '@/constants';
 import { getCookies } from '@/utils';
@@ -37,7 +38,18 @@ export function MainPage() {
           )}
         </div>
       </div>
-      <img src={MAIN_IMAGE} alt="main" className={styles.mainImage} />
+      <picture>
+        <source
+          type="image/webp"
+          srcSet={MAIN_IMAGE_WEBP}
+          className={styles.mainImage}
+        />
+        <img
+          src={MAIN_IMAGE_PNG}
+          alt="메인 이미지"
+          className={styles.mainImage}
+        />
+      </picture>
     </div>
   );
 }
