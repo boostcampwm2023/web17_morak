502e7665c25f3d7a37649c77517fe172194d60dc
Jiseung <js43o@naver.com>
2023-12-13 15:16:14 +0900

    ♿️ 모각코 리스트 페이지 시맨틱 태그 적용

--- app/frontend/src/components/MogacoItem/index.tsx
+++ app/frontend/src/components/MogacoItem/index.tsx
@@ -37,24 +37,27 @@ export function MogacoItem({
 
   return (
     <NavLink to={`/mogaco/${id}`} className={styles.container}>
-      <div className={styles.titleArea}>
+      <h2 className={styles.titleArea}>
         {MogacoLabel}
         <div className={styles.title}>{title}</div>
-      </div>
-      <div className={styles.group}>{groupTitle}</div>
+      </h2>
+      <span className={styles.group}>{groupTitle}</span>
       <div className={styles.content}>
-        <div className={styles.detail}>{contents}</div>
+        <p className={styles.detail}>{contents}</p>
         <div className={styles.info}>
-          <div className={styles.infoContent}>
+          <address className={styles.infoContent}>
             <Map className={styles.icon} width={20} height={20} />
-            <div className={styles.infoText}>{address}</div>
-          </div>
-          <div className={styles.infoContent}>
+            <span className={styles.infoText}>{address}</span>
+          </address>
+          <time
+            className={styles.infoContent}
+            dateTime={dayjs(date).format('YYYY-MM-DD HH:MM')}
+          >
             <Calendar className={styles.icon} width={20} height={20} />
-            <div className={styles.infoText}>
+            <span className={styles.infoText}>
               {dayjs(date).format('YYYY/MM/DD HH:mm~')}
-            </div>
-          </div>
+            </span>
+          </time>
         </div>
       </div>
     </NavLink>
