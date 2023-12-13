d4bff3d2a0da27ddd5326c07309956eb4bda3762
Jiseung <js43o@naver.com>
2023-12-13 16:26:49 +0900

    ♿️ Pagination 요소별 aria-label 속성 추가

--- packages/morak-ui/src/components/Pagination/index.tsx
+++ packages/morak-ui/src/components/Pagination/index.tsx
@@ -24,43 +24,52 @@ export function Pagination({
 }: PaginationProps) {
   const [start, end] = get10UnitRange(currentPage);
   const lastPageNum = pageCount ? Math.min(end, pageCount) : end;
-  const array = createRangeArray(start, lastPageNum);
+  const pageNumbers = createRangeArray(start, lastPageNum);
   const arrow = <Arrow width={16} height={16} fill={grayscale200} />;
   const isFirstPage = currentPage === 1;
   const isLastPage = currentPage === lastPageNum;
 
   return (
-    <div className={`${styles.container} ${className}`}>
-      <button
-        type="button"
-        onClick={onClickPrev}
-        disabled={isFirstPage}
-        aria-label="prev-page"
-      >
-        {arrow}
-      </button>
-      {array.map((page) => (
-        <button
-          onClick={onClickItem}
-          className={`${styles.page} ${
-            currentPage === page ? styles.current : ''
-          }`}
-          type="button"
-          key={page}
-          value={page}
-        >
-          {page}
-        </button>
-      ))}
-      <button
-        type="button"
-        className={styles.rotateArrow}
-        onClick={onClickNext}
-        disabled={isLastPage}
-        aria-label="next-page"
-      >
-        {arrow}
-      </button>
-    </div>
+    <nav role="navigation" aria-label="페이지 선택">
+      <ul className={`${styles.container} ${className}`}>
+        <li>
+          <button
+            type="button"
+            onClick={onClickPrev}
+            disabled={isFirstPage}
+            aria-label="이전 페이지"
+          >
+            {arrow}
+          </button>
+        </li>
+        {pageNumbers.map((page: number) => (
+          <li key={page}>
+            <button
+              onClick={onClickItem}
+              className={`${styles.page} ${
+                currentPage === page ? styles.current : ''
+              }`}
+              type="button"
+              value={page}
+              aria-current={currentPage === page}
+              aria-label={`${page} 페이지`}
+            >
+              {page}
+            </button>
+          </li>
+        ))}
+        <li>
+          <button
+            type="button"
+            className={styles.rotateArrow}
+            onClick={onClickNext}
+            disabled={isLastPage}
+            aria-label="다음 페이지"
+          >
+            {arrow}
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 }
