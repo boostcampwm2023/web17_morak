c173add6a050de647fb896bec6ab1c3c4620cccb
DongHyeok Lim <jh07050@gmail.com>
2024-01-02 21:03:56 +0900

    ✅ app.e2e 테스트

--- app/backend/test/app.e2e-spec.ts
+++ app/backend/test/app.e2e-spec.ts
@@ -1,21 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('hello Morak');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
