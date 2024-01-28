4efd47be2d21ff560cdd5b7e899455f15f271791
ttaerrim <ltr0121@naver.com>
2024-01-28 16:03:30 +0900

    🔨 react-hook-form `useForm` 사용
    - 임시 GroupCreate 타입 추가
    - 공개/비공개 그룹 타입 useForm 사용해서 선택하도록 변경

--- app/frontend/src/pages/Groups/Create/index.tsx
+++ app/frontend/src/pages/Groups/Create/index.tsx
@@ -1,35 +1,73 @@
+import { Controller, useForm } from 'react-hook-form';
+
 import { TextLabel, Button } from '@morak/ui';
 
 import { FormInput } from '@/components';
 
 import * as styles from './index.css';
 
+type GroupJoin = 'need-approve' | 'need-code';
+type GroupCreate = {
+  name: string;
+  type: 'public-group' | 'private-group';
+  joinType: GroupJoin[];
+};
 export function GroupCreatePage() {
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<GroupCreate>({
+    defaultValues: {
+      name: '',
+      type: 'public-group',
+      joinType: ['need-approve'],
+    },
+    mode: 'all',
+  });
   return (
-    <form className={styles.container}>
-      <FormInput label="그룹명" required value="dd" />
+    <form
+      className={styles.container}
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
+      <FormInput label="그룹명" required />
       <div className={styles.inputWrapper}>
         <TextLabel label="그룹 유형" required />
         <div className={styles.groupWrapper}>
-          <label className={styles.inputField} htmlFor="public-group">
-            <input type="radio" name="group" id="public-group" />
-            공개 그룹
-          </label>
-          <label className={styles.inputField} htmlFor="private-group">
-            <input type="radio" name="group" id="private-group" />
-            비공개 그룹
-          </label>
+          <Controller
+            control={control}
+            name="type"
+            render={({ field: { onChange, value } }) => (
+              <>
+                <label className={styles.inputField} htmlFor="public-group">
+                  <input
+                    type="radio"
+                    name="group"
+                    id="public-group"
+                    checked={value === 'public-group'}
+                    onChange={(e) => onChange(e.target.id)}
+                  />
+                  공개 그룹
+                </label>
+                <label className={styles.inputField} htmlFor="private-group">
+                  <input
+                    type="radio"
+                    name="group"
+                    id="private-group"
+                    checked={value === 'private-group'}
+                    onChange={(e) => onChange(e.target.id)}
+                  />
+                  비공개 그룹
+                </label>
+              </>
+            )}
+          />
         </div>
       </div>
       <div className={styles.inputWrapper}>
         <TextLabel label="가입 방식" required />
         <label className={styles.inputField} htmlFor="need-approve">
-          <input
-            type="checkbox"
-            id="need-approve"
-            name="need-approve"
-            checked
-          />
+          <input type="checkbox" id="need-approve" name="need-approve" />
           그룹장의 가입 승인 필요
         </label>
         <label className={styles.inputField} htmlFor="need-code">
@@ -37,7 +75,14 @@ export function GroupCreatePage() {
           참여 코드로 바로 가입
         </label>
       </div>
-      <Button type="submit" theme="primary" shape="fill" size="large" fullWidth>
+      <Button
+        type="submit"
+        theme="primary"
+        shape="fill"
+        size="large"
+        fullWidth
+        disabled={!isValid}
+      >
         확인
       </Button>
     </form>
