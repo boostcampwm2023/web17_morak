6b15bf1a3ff63c91b9bd71a5bad2a3a6ed41f6a6
DongHyeok Lim <jh07050@gmail.com>
2024-01-29 18:19:00 +0900

    ✨ groupTypeId 추가사항 dto 추가

--- app/backend/src/groups/dto/create-groups.dto.ts
+++ app/backend/src/groups/dto/create-groups.dto.ts
@@ -1,9 +1,12 @@
 import { RequestGroupsDto } from '@morak/apitype/dto/request/groups';
-import { IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateGroupsDto implements RequestGroupsDto {
   @ApiProperty({ description: 'Title of the Group', example: '부스트캠프 웹·모바일 8기' })
   @IsNotEmpty()
   title: string;
+
+  @IsInt()
+  groupTypeId: number;
 }
--- packages/apitype/dto/request/groups.ts
+++ packages/apitype/dto/request/groups.ts
@@ -1,3 +1,4 @@
 export interface RequestGroupsDto {
   title: string;
+  groupTypeId: number;
 }
