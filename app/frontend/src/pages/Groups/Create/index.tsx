65a6c02eaa93b3d7c682503a83bbd48f4a8656d3
ttaerrim <ltr0121@naver.com>
2024-02-01 15:01:54 +0900

    🔨 그룹명 useController 적용

--- app/frontend/src/pages/Groups/Create/index.tsx
+++ app/frontend/src/pages/Groups/Create/index.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form';
+import { useController, useForm } from 'react-hook-form';
 
 import { TextLabel, Button } from '@morak/ui';
 
@@ -22,13 +22,27 @@ export function GroupCreatePage() {
     mode: 'all',
   });
 
+  const {
+    field: { value: nameValue, onChange: onChangeName },
+  } = useController({
+    name: 'name',
+    control,
+    rules: {
+      required: true,
+    },
+  });
   return (
     <form
       className={styles.container}
       // TODO: POST 요청
       onSubmit={handleSubmit((data) => console.log(data))}
     >
-      <FormInput label="그룹명" required />
+      <FormInput
+        label="그룹명"
+        required
+        value={nameValue}
+        onChange={onChangeName}
+      />
       <div className={styles.inputWrapper}>
         <TextLabel label="그룹 유형" required />
         <GroupTypeRadio control={control} />
